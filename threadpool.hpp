e200e58479ba1e9f3196a339a6e3730ddc114a28
Stanislav Fomichev <stfomichev@yandex-team.ru>
2014-07-29 22:36:55 +0400

    threadpool: add native pthread mode and use it by default
    Signed-off-by: Stanislav Fomichev <stfomichev@yandex-team.ru>

--- threadpool.hpp
+++ threadpool.hpp
@@ -5,29 +5,65 @@
 #include <queue>
 #include <condition_variable>
 
+#define USE_CXX11 0
+
+#if !USE_CXX11
+#include <pthread.h>
+#endif
+
 class TThreadPool {
 public:
     TThreadPool(size_t size) : run(true)
     {
+#if USE_CXX11
         for (size_t i = 0; i < size; i++)
             workers.emplace_back([this]{ worker_fn(); });
+#else
+        workers.resize(size);
+        for (size_t i = 0; i < size; i++)
+            if (pthread_create(&workers[i], nullptr, TThreadPool::worker_fn, this))
+                throw "TThreadPool: can't create worker thread";
+
+        if (pthread_mutex_init(&mutex, NULL))
+                throw "TThreadPool: can't initialize mutex";
+        if (pthread_cond_init(&cond, NULL))
+                throw "TThreadPool: can't initialize conditional variable";
+#endif
     }
 
     ~TThreadPool()
     {
+#if USE_CXX11
         {
             std::unique_lock<std::mutex> lock(mutex);
             run = false;
         }
 
-        condition.notify_all();
+        cv.notify_all();
 
         for (size_t i = 0; i < workers.size(); i++)
             workers[i].join();
+#else
+        if (pthread_mutex_lock(&mutex))
+            throw "TThreadPool: can't lock mutex";
+        run = false;
+        if (pthread_cond_broadcast(&cond))
+            throw "TThreadPool: can't wake up all worker threads";
+        if (pthread_mutex_unlock(&mutex))
+            throw "TThreadPool: can't unlock mutex";
+
+        for (size_t i = 0; i < workers.size(); i++)
+            if (pthread_join(workers[i], nullptr))
+                throw "TThreadPool: can't join worker thread";
+
+        (void)pthread_mutex_destroy(&mutex);
+        (void)pthread_cond_destroy(&cond);
+#endif
     }
 
     bool Enqueue(std::function<void()> f)
     {
+#if USE_CXX11
         std::unique_lock<std::mutex> lock(mutex);
 
         if (!run)
@@ -35,25 +71,50 @@ public:
 
         jobs.push(f);
 
-        condition.notify_one();
+        cv.notify_one();
+#else
+        if (pthread_mutex_lock(&mutex))
+            throw "TThreadPool: can't lock mutex";
+
+        if (!run) {
+            if (pthread_mutex_unlock(&mutex))
+                throw "TThreadPool: can't unlock mutex";
+            return false;
+        }
+
+        jobs.push(f);
+
+        if (pthread_cond_signal(&cond))
+            throw "TThreadPool: can't wake worker thread";
+
+        if (pthread_mutex_unlock(&mutex))
+            throw "TThreadPool: can't unlock mutex";
+#endif
 
         return true;
     }
 private:
-    std::vector< std::thread > workers;
-    std::queue< std::function<void()> > jobs;
-
-    std::mutex mutex;
-    std::condition_variable condition;
     bool run;
+    std::queue< std::function<void()> > jobs;
+#if USE_CXX11
+    std::vector< std::thread > workers;
+    std::mutex mutex;
+    std::condition_variable cv;
+#else
+    std::vector<pthread_t> workers;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+#endif
 
+#if USE_CXX11
     void worker_fn()
     {
         while (true) {
             std::unique_lock<std::mutex> lock(mutex);
 
-            while (run && jobs.empty())
-                condition.wait(lock);
+            while (run && jobs.empty()) {
+                cv.wait(lock);
+            }
 
             if (!run) {
                 lock.unlock();
@@ -66,6 +127,34 @@ private:
             f();
         }
     }
+#else
+    static void *worker_fn(void *arg)
+    {
+        TThreadPool *pool = static_cast<TThreadPool *>(arg);
+
+        while (true) {
+            if (pthread_mutex_lock(&pool->mutex))
+                throw "TThreadPool: can't lock mutex";
+
+            while (pool->run && pool->jobs.empty()) {
+                if (pthread_cond_wait(&pool->cond, &pool->mutex))
+                    throw "TThreadPool: can't wait on conditional variable";
+            }
+
+            if (!pool->run) {
+                if (pthread_mutex_unlock(&pool->mutex))
+                    throw "TThreadPool: can't unlock mutex";
+                return nullptr;
+            }
+
+            auto f(pool->jobs.front());
+            pool->jobs.pop();
+            if (pthread_mutex_unlock(&pool->mutex))
+                throw "TThreadPool: can't unlock mutex";
+            f();
+        }
+    }
+#endif
 };
 
 #endif
