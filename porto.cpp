8940fd395ccb36f1e7a7da76635c7b4049320b45
Stanislav Fomichev <stfomichev@yandex-team.ru>
2014-07-30 16:14:46 +0400

    porto: add generic commands handling
    Signed-off-by: Stanislav Fomichev <stfomichev@yandex-team.ru>

--- porto.cpp
+++ porto.cpp
@@ -10,6 +10,20 @@ extern "C" {
 #include <sys/un.h>
 }
 
+class ICmd {
+protected:
+    int fd;
+
+public:
+    void SetFd(int fd) { this->fd = fd; }
+    virtual string GetName() = 0;
+    virtual string GetUsage() = 0;
+    virtual string GetDescription() = 0;
+    virtual int Execute(int argc, char *argv[]) = 0;
+};
+
+static vector<ICmd *> commands;
+
 static int ConnectToRpcServer(const char *path)
 {
     int sfd;
@@ -36,44 +50,156 @@ static int ConnectToRpcServer(const char *path)
     return sfd;
 }
 
+class THelpCmd : public ICmd {
+public:
+    string GetName()
+    {
+        return "help";
+    }
+
+    string GetUsage()
+    {
+        return "[command]";
+    }
+
+    string GetDescription()
+    {
+        return "print help message for command";
+    }
+
+    void Usage(const char *name)
+    {
+        cout << "usage: " << name << " <command> [<args>]" << endl;
+        cout << endl;
+        cout << "list of commands:" << endl;
+        for (ICmd *cmd : commands)
+            cout << " " << cmd->GetName() <<
+                "\t\t" << cmd->GetDescription()<<endl;
+    }
+
+    int Execute(int argc, char *argv[])
+    {
+        if (argc <= 2) {
+            Usage(argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        string name = argv[2];
+        for (ICmd *cmd : commands) {
+            if (cmd->GetName() == name) {
+                cout << "usage: " << argv[0] << " " << name << " " << cmd->GetUsage() << endl;
+                cout << endl;
+                cout << cmd->GetDescription() << endl;
+
+                return EXIT_SUCCESS;
+            }
+        }
+
+        Usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+};
+
+static void Usage(char *name, const char *command)
+{
+    ICmd *cmd = new THelpCmd();
+    char *argv[] = { name, (char *)"help", (char *)command, NULL };
+
+    cmd->Execute(command ? 3 : 1, argv);
+}
+
+static bool NeedHelp(int argc, char *argv[], bool canBeEmpty)
+{
+    if (argc >= 2) {
+        string arg = argv[2];
+        if (arg == "-h" || arg == "--help" || arg == "help")
+            return true;
+        else
+            return false;
+    }
+    return canBeEmpty ? false : true;
+}
+
+class TSendCmd : public ICmd {
+public:
+    string GetName()
+    {
+        return "send";
+    }
+
+    string GetUsage()
+    {
+        return "<message>";
+    }
+
+    string GetDescription()
+    {
+        return "send raw protobuf message";
+    }
+
+    int Execute(int argc, char *argv[])
+    {
+        string s;
+        stringstream msg;
+
+        if (NeedHelp(argc, argv, false)) {
+            Usage(argv[0], GetName().c_str());
+            return EXIT_FAILURE;
+        }
+
+        int fd = ConnectToRpcServer(RPC_SOCK_PATH);
+        if (fd < 0) {
+            std::cerr<<"Can't connect to RPC server"<<std::endl;
+            return fd;
+        }
+
+        std::vector<std::string> args(argv + 1, argv + argc);
+        copy(args.begin(), args.end(), ostream_iterator<string>(msg, " "));
+
+        rpc::TContainerRequest request;
+        if (!google::protobuf::TextFormat::ParseFromString(msg.str(), &request) ||
+            !request.IsInitialized()) {
+            close(fd);
+            return -1;
+        }
+
+        google::protobuf::io::FileInputStream pist(fd);
+        google::protobuf::io::FileOutputStream post(fd);
+
+        rpc::TContainerResponse response;
+
+        writeDelimitedTo(request, &post);
+        post.Flush();
+
+        if (readDelimitedFrom(&pist, &response)) {
+            google::protobuf::TextFormat::PrintToString(response, &s);
+            cout<<s<<endl;
+        }
+
+        close(fd);
+        return 0;
+    }
+};
+
 int main(int argc, char *argv[])
 {
-    string s;
-    int fd = ConnectToRpcServer(RPC_SOCK_PATH);
-
-    if (fd < 0) {
-        std::cerr<<"Can't connect to RPC server"<<std::endl;
-        return fd;
-    }
-
     if (argc <= 1) {
-        std::cerr<<"Please specify message!";
-        return -1;
+        Usage(argv[0], NULL);
+        return EXIT_FAILURE;
     }
 
-    stringstream msg;
-    std::vector<std::string> args(argv + 1, argv + argc);
-    copy(args.begin(), args.end(), ostream_iterator<string>(msg, " "));
-
-    rpc::TContainerRequest request;
-    if (!google::protobuf::TextFormat::ParseFromString(msg.str(), &request) ||
-        !request.IsInitialized())
-        return -1;
-
-    google::protobuf::io::FileInputStream pist(fd);
-    google::protobuf::io::FileOutputStream post(fd);
-
-    rpc::TContainerResponse response;
-
-    writeDelimitedTo(request, &post);
-    post.Flush();
-
-    if (readDelimitedFrom(&pist, &response)) {
-        google::protobuf::TextFormat::PrintToString(response, &s);
-        cout<<s<<endl;
+    string name = argv[1];
+    if (name == "-h" || name == "--help") {
+        Usage(argv[0], NULL);
+        return EXIT_FAILURE;
     }
 
-    close(fd);
+    commands.push_back(new THelpCmd());
+    commands.push_back(new TSendCmd());
 
-    return 0;
+    for (ICmd *cmd : commands)
+        if (cmd->GetName() == name)
+            return cmd->Execute(argc, argv);
+
+    return EXIT_FAILURE;
 }
