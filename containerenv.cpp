4ad4a87a1e22b32f407135c0c2a7295bb263517a
Roman Gushchin <klamm@yandex-team.ru>
2014-08-01 14:37:40 +0400

    Implement: TContainerEnv::Create() & TContainerEnv::Enter()

--- containerenv.cpp
+++ containerenv.cpp
@@ -1,8 +1,12 @@
 #include "containerenv.hpp"
 
 void TContainerEnv::Create() {
+    for (auto cg : leaf_cgroups)
+        cg.Create();
 }
 
 void TContainerEnv::Enter() {
+    for (auto cg : leaf_cgroups)
+        cg.Enter();
 }
 
