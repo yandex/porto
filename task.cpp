777d98abdffb787f5e9acddc7be10b11fcde6286
Stanislav Fomichev <stfomichev@yandex-team.ru>
2015-01-15 11:42:37 +0300

    teamcity_test: add dummy network interface
    Signed-off-by: Stanislav Fomichev <stfomichev@yandex-team.ru>

--- task.cpp
+++ task.cpp
@@ -269,7 +269,7 @@ TError TTask::ChildExec() {
     auto envp = Env->GetEnvp();
     execvpe(result.we_wordv[0], (char *const *)result.we_wordv, (char *const *)envp);
 
-    return TError(EError::Unknown, errno, string("execvpe(") + result.we_wordv[0] + ") " + std::to_string(result.we_wordc) + "/" + std::to_string(Env->Environ.size()));
+    return TError(EError::Unknown, errno, string("execvpe(") + result.we_wordv[0] + ", " + std::to_string(result.we_wordc) + ", " + std::to_string(Env->Environ.size()) + ")");
 }
 
 TError TTask::ChildBindDns() {
