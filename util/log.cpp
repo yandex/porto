b27693261225f23209eb4db72824dc1c92e1a8d5
Roman Gushchin <klamm@yandex-team.ru>
2015-06-03 14:47:05 +0300

    log: fix logging for set property request
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>

--- util/log.cpp
+++ util/log.cpp
@@ -187,7 +187,7 @@ std::string RequestAsString(const rpc::TContainerRequest &req) {
     else if (req.has_getproperty())
         return "pget "  + req.getproperty().name() + " " + req.getproperty().property();
     else if (req.has_setproperty())
-        return "pset " + req.setproperty().name() + " " + req.getproperty().property();
+        return "pset " + req.setproperty().name() + " " + req.setproperty().property();
     else if (req.has_getdata())
         return "dget " + req.getdata().name() + " " + req.getdata().data();
     else if (req.has_get()) {
