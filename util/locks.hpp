0e71ac8f3a260bf2573c2010ed1d620e48795596
Roman Gushchin <klamm@yandex-team.ru>
2015-07-01 14:21:20 +0300

    locking: add some checks
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>

--- util/locks.hpp
+++ util/locks.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "util/log.hpp"
+
 constexpr bool AllowHolderUnlock = false;
 
 typedef std::unique_lock<std::mutex> TScopedLock;
@@ -7,10 +9,12 @@ typedef std::unique_lock<std::mutex> TScopedLock;
 class TScopedUnlock : public TNonCopyable {
 public:
     TScopedUnlock(TScopedLock &lock) {
+        PORTO_ASSERT(lock);
         Lock = &lock;
         Lock->unlock();
     }
     ~TScopedUnlock() {
+        PORTO_ASSERT(!*Lock);
         Lock->lock();
     }
 private:
@@ -23,6 +27,7 @@ public:
         return TScopedLock(Mutex);
     }
     TScopedLock NestScopedLock(TScopedLock &lock) {
+        PORTO_ASSERT(lock);
         TScopedUnlock unlock(lock);
         return TScopedLock(Mutex);
     }
