81f64f4f893f085b2442248f7c73bfe2cc0d29a9
Roman Gushchin <klamm@yandex-team.ru>
2015-07-02 20:04:24 +0300

    tests: python phaser v2
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>

--- test/threads.py
+++ test/threads.py
@@ -1,91 +1,98 @@
 from porto import *
 import time
 import sys
+import os
 import signal
 import multiprocessing
 import traceback
 
 ################################################################################
 
-def thread1(conn): # listener (monitoring, portoctl list, ...)
-#    print(conn.List())
-    print(conn.Get(conn.List(), ['state', 'cpu_usage', 'memory_usage']))
+def iss(conn, this_script, top): # top-level executor
+    if top:
+        monitoring = conn.Create('monitoring')
+        monitoring.SetProperty('command', 'python ' + this_script + ' monitoring')
+        monitoring.SetProperty('isolate', 'false')
+        monitoring.Start()
+
+        cloud = conn.Create('cloud')
+        cloud.SetProperty('command', 'python ' + this_script + ' cloud')
+        cloud.SetProperty('porto_namespace', 'cloud/')
+        cloud.Start()
+
+        iss2 = conn.Create('iss2')
+        iss2.SetProperty('command', 'python ' + this_script + ' iss')
+        iss2.SetProperty('porto_namespace', 'iss2/')
+        iss2.Start()
+
+    jobs = []
+    for x in range(0, 20):
+        top = conn.Create('search' + str(x))
+        top.SetProperty('isolate', 'true')
+        c = conn.Create('search' + str(x) + '/hook')
+        c.SetProperty('command', 'sleep ' + str(x))
+        c.SetProperty('isolate', 'true')
+        c.Start()
+        jobs.append(top)
+
+    time.sleep(20)
+
+    for x in range(0, 20):
+        jobs[x].Stop()
+        conn.Destroy('search' + str(x))
+
+    if top:
+        conn.Stop('cloud')
+        conn.Destroy('cloud')
+        conn.Stop('iss2')
+        conn.Destroy('iss2')
+        conn.Stop('monitoring')
+        conn.Destroy('monitoring')
+
+def monitoring(conn, this_script, top):
+    conn.Get(conn.List(), ['state', 'cpu_usage', 'memory_usage'])
     time.sleep(1)
 
-def thread2(conn): # top-level executor
-    name = 'iss'
-    sub1 = 'iss/qloud'
-    sub2 = 'iss/base'
+def cloud(conn, this_script, top):
+    jobs = []
+    for x in range(0, 10):
+        c = conn.Create('job' + str(x))
+        c.SetProperty('command', 'sleep 5')
+        c.Start()
+        jobs.append(c)
+        time.sleep(1)
 
-    for container in [ sub2, sub1, name ]:
-        try:
-            conn.Destroy(container)
-            conn.Destroy(container)
-            conn.Destroy(container)
-        except exceptions.ContainerDoesNotExist:
-            pass
-
-    print('create ' + name)
-    c = conn.Create(name)
-    c.SetProperty('command', 'sleep 1000000')
-    c.Start()
-
-    print('create ' + sub1)
-    c2 = conn.Create(sub1)
-    c2.SetProperty('command', 'sleep 1000000')
-    c2.Start()
-
-    print('create ' + sub2)
-    c3 = conn.Create(sub2)
-    c3.SetProperty('command', 'sleep 1')
-    c3.Start()
-
-    time.sleep(5)
-
-    print('stop ' + name)
-    c.Stop()
-
-    conn.Destroy(sub2)
-    conn.Destroy(sub1)
-    conn.Destroy(name)
-
-def thread3(conn): # sub-executor
-    time.sleep(1)
-
-def thread4(conn): # sibling executor
-    time.sleep(1)
-
-tests = [thread1, thread2, thread3, thread4]
+    for x in range(0, 10):
+        jobs[x].Stop()
+        conn.Destroy('job' + str(x))
+        time.sleep(1)
 
 ################################################################################
 
-def run_thread(thread):
-    conn = Connection()
-    conn.connect()
+def run_thread(thread, this_script, top):
+    try:
+        conn = Connection()
+        conn.connect()
 
-    while True:
-        thread(conn)
-
-    conn.disconnect()
+        while True:
+            print(thread)
+            globals()[thread](conn, this_script, top)
+    except:
+        print('Error in ' + thread)
+        sys.exit(-1)
 
 def init_worker():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(len(tests), init_worker)
-    result = pool.map_async(run_thread, tests)
-
     try:
-        result.get(60)
-        print "Test failed"
-        sys.exit(-1)
-    except KeyboardInterrupt:
-        print "Terminated by user"
-        pool.terminate()
-        pool.join()
-        sys.exit(-1)
-    except multiprocessing.TimeoutError:
-        pool.terminate()
-        pool.join()
-        print "Test passed"
+        if len(sys.argv) > 1:
+            run_thread(sys.argv[1], sys.argv[0], False)
+        else:
+            run_thread('iss', os.getcwd() + '/' + sys.argv[0], True)
         sys.exit(0)
+    except KeyboardInterrupt:
+        print 'Terminated by user'
+        pool.terminate()
+        pool.join()
+        sys.exit(-1)
