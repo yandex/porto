c804351a31b6fc7b786a1a1d94fcae0eb22968b7
Roman Gushchin <klamm@yandex-team.ru>
2015-07-03 13:20:27 +0300

    tests: enhance python phaser (check states and handle errors)
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>

--- test/threads.py
+++ test/threads.py
@@ -5,21 +5,30 @@ import os
 import signal
 import multiprocessing
 import traceback
+import random
 
 ################################################################################
 
+def check_running(conn, container):
+    state = conn.GetData(container, 'state')
+    if state != 'running':
+        raise exceptions.InvalidState(container + ' in state ' + state)
+
 def iss(conn, this_script, top): # top-level executor
     if top:
+        print('start monitoring...')
         monitoring = conn.Create('monitoring')
         monitoring.SetProperty('command', 'python ' + this_script + ' monitoring')
         monitoring.SetProperty('isolate', 'false')
         monitoring.Start()
 
+        print('start cloud')
         cloud = conn.Create('cloud')
         cloud.SetProperty('command', 'python ' + this_script + ' cloud')
         cloud.SetProperty('porto_namespace', 'cloud/')
         cloud.Start()
 
+        print('start iss2')
         iss2 = conn.Create('iss2')
         iss2.SetProperty('command', 'python ' + this_script + ' iss')
         iss2.SetProperty('porto_namespace', 'iss2/')
@@ -27,59 +36,67 @@ def iss(conn, this_script, top): # top-level executor
 
     jobs = []
     for x in range(0, 20):
+        print('start search' + str(x))
         top = conn.Create('search' + str(x))
         top.SetProperty('isolate', 'true')
         c = conn.Create('search' + str(x) + '/hook')
-        c.SetProperty('command', 'sleep ' + str(x))
+        c.SetProperty('command', 'sleep ' + str(random.randint(1, 20)))
         c.SetProperty('isolate', 'true')
         c.Start()
         jobs.append(top)
 
-    time.sleep(20)
+    time.sleep(random.randint(15, 30))
 
     for x in range(0, 20):
+        print('stop search' + str(x))
         jobs[x].Stop()
+        print('destroy search' + str(x))
         conn.Destroy('search' + str(x))
 
     if top:
+        check_running(conn, 'cloud')
+        print('stop and destroy cloud')
         conn.Stop('cloud')
         conn.Destroy('cloud')
+
+        check_running(conn, 'iss2')
+        print('stop and destroy iss2')
         conn.Stop('iss2')
         conn.Destroy('iss2')
+
+        check_running(conn, 'monitoring')
+        print('stop and destroy monitoring')
         conn.Stop('monitoring')
         conn.Destroy('monitoring')
 
 def monitoring(conn, this_script, top):
-    conn.Get(conn.List(), ['state', 'cpu_usage', 'memory_usage'])
-    time.sleep(1)
+    print(conn.Get(conn.List(), ['state', 'cpu_usage', 'memory_usage']))
+    time.sleep(random.randint(0, 1))
 
 def cloud(conn, this_script, top):
     jobs = []
     for x in range(0, 10):
+        print('create and start job' + str(x))
         c = conn.Create('job' + str(x))
-        c.SetProperty('command', 'sleep 5')
+        c.SetProperty('command', 'sleep ' + str(random.randint(3, 10)))
         c.Start()
         jobs.append(c)
-        time.sleep(1)
+        time.sleep(random.randint(0, 1))
 
     for x in range(0, 10):
+        print('stop and destroy job' + str(x))
         jobs[x].Stop()
         conn.Destroy('job' + str(x))
-        time.sleep(1)
+        time.sleep(random.randint(0, 1))
 
 ################################################################################
 
 def run_thread(thread, this_script, top):
-    try:
-        conn = Connection()
-        conn.connect()
+    conn = Connection()
+    conn.connect()
 
-        while True:
-            print(thread)
-            globals()[thread](conn, this_script, top)
-    except:
-        print('Error in ' + thread)
-        sys.exit(-1)
+    while True:
+        globals()[thread](conn, this_script, top)
 
 def init_worker():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -93,6 +110,4 @@ if __name__ == '__main__':
         sys.exit(0)
     except KeyboardInterrupt:
         print 'Terminated by user'
-        pool.terminate()
-        pool.join()
-        sys.exit(-1)
+        raise
