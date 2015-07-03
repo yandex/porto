49eef882c47606c156662353822030da9a3c4358
Roman Gushchin <klamm@yandex-team.ru>
2015-07-03 14:57:26 +0300

    tests: enhance python phaser
    Signed-off-by: Roman Gushchin <klamm@yandex-team.ru>

--- test/threads.py
+++ test/threads.py
@@ -14,26 +14,45 @@ def check_running(conn, container):
     if state != 'running':
         raise exceptions.InvalidState(container + ' in state ' + state)
 
-def iss(conn, this_script, top): # top-level executor
-    if top:
-        print('start monitoring...')
-        monitoring = conn.Create('monitoring')
-        monitoring.SetProperty('command', 'python ' + this_script + ' monitoring')
-        monitoring.SetProperty('isolate', 'false')
-        monitoring.Start()
+def top_iss(conn, this_script):  # top-level executor
+    print('start monitoring...')
+    monitoring = conn.Create('monitoring')
+    monitoring.SetProperty('command', 'python ' + this_script + ' monitoring')
+    monitoring.SetProperty('isolate', 'false')
+    monitoring.Start()
 
-        print('start cloud')
-        cloud = conn.Create('cloud')
-        cloud.SetProperty('command', 'python ' + this_script + ' cloud')
-        cloud.SetProperty('porto_namespace', 'cloud/')
-        cloud.Start()
+    print('start cloud')
+    cloud = conn.Create('cloud')
+    cloud.SetProperty('command', 'python ' + this_script + ' cloud')
+    cloud.SetProperty('porto_namespace', 'cloud/')
+    cloud.Start()
 
-        print('start iss2')
-        iss2 = conn.Create('iss2')
-        iss2.SetProperty('command', 'python ' + this_script + ' iss')
-        iss2.SetProperty('porto_namespace', 'iss2/')
-        iss2.Start()
+    print('start iss2')
+    iss2 = conn.Create('iss2')
+    iss2.SetProperty('command', 'python ' + this_script + ' iss')
+    iss2.SetProperty('porto_namespace', 'iss2/')
+    iss2.Start()
 
+    iss(conn, this_script)
+
+    time.sleep(random.randint(10, 30))
+
+    check_running(conn, 'cloud')
+    print('stop and destroy cloud')
+    conn.Stop('cloud')
+    conn.Destroy('cloud')
+
+    check_running(conn, 'iss2')
+    print('stop and destroy iss2')
+    conn.Stop('iss2')
+    conn.Destroy('iss2')
+
+    check_running(conn, 'monitoring')
+    print('stop and destroy monitoring')
+    conn.Stop('monitoring')
+    conn.Destroy('monitoring')
+
+def iss(conn, this_script): #sub-executor
     jobs = []
     for x in range(0, 20):
         print('start search' + str(x))
@@ -45,7 +64,7 @@ def iss(conn, this_script, top): # top-level executor
         c.Start()
         jobs.append(top)
 
-    time.sleep(random.randint(15, 30))
+    time.sleep(random.randint(1, 5))
 
     for x in range(0, 20):
         print('stop search' + str(x))
@@ -53,27 +72,11 @@ def iss(conn, this_script, top): # top-level executor
         print('destroy search' + str(x))
         conn.Destroy('search' + str(x))
 
-    if top:
-        check_running(conn, 'cloud')
-        print('stop and destroy cloud')
-        conn.Stop('cloud')
-        conn.Destroy('cloud')
-
-        check_running(conn, 'iss2')
-        print('stop and destroy iss2')
-        conn.Stop('iss2')
-        conn.Destroy('iss2')
-
-        check_running(conn, 'monitoring')
-        print('stop and destroy monitoring')
-        conn.Stop('monitoring')
-        conn.Destroy('monitoring')
-
-def monitoring(conn, this_script, top):
+def monitoring(conn, this_script):
     print(conn.Get(conn.List(), ['state', 'cpu_usage', 'memory_usage']))
     time.sleep(random.randint(0, 1))
 
-def cloud(conn, this_script, top):
+def cloud(conn, this_script):
     jobs = []
     for x in range(0, 10):
         print('create and start job' + str(x))
@@ -91,12 +94,12 @@ def cloud(conn, this_script, top):
 
 ################################################################################
 
-def run_thread(thread, this_script, top):
-    conn = Connection()
+def run_thread(thread, this_script):
+    conn = Connection(socket_path='/run/portod.socket', timeout=30)
     conn.connect()
 
     while True:
-        globals()[thread](conn, this_script, top)
+        globals()[thread](conn, this_script)
 
 def init_worker():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -104,9 +107,9 @@ def init_worker():
 if __name__ == '__main__':
     try:
         if len(sys.argv) > 1:
-            run_thread(sys.argv[1], sys.argv[0], False)
+            run_thread(sys.argv[1], sys.argv[0])
         else:
-            run_thread('iss', os.getcwd() + '/' + sys.argv[0], True)
+            run_thread('top_iss', os.getcwd() + '/' + sys.argv[0])
         sys.exit(0)
     except KeyboardInterrupt:
         print 'Terminated by user'
