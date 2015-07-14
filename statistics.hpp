52c0e4baa4a33fa13bb81b91b28b26ab03a79739
Stanislav Fomichev <stfomichev@yandex-team.ru>
2015-07-14 21:37:20 +0300

    add missing statistics.hpp
    Signed-off-by: Stanislav Fomichev <stfomichev@yandex-team.ru>

---
+++ statistics.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <atomic>
+
+struct TStatistics {
+    std::atomic<uint64_t> Spawned;
+    std::atomic<uint64_t> Errors;
+    std::atomic<uint64_t> Warns;
+    std::atomic<uint64_t> MasterStarted;
+    std::atomic<uint64_t> SlaveStarted;
+    std::atomic<uint64_t> QueuedStatuses;
+    std::atomic<uint64_t> QueuedEvents;
+    std::atomic<uint64_t> Created;
+    std::atomic<uint64_t> Started;
+    std::atomic<uint64_t> RemoveDead;
+    std::atomic<int> SlaveTimeoutMs;
+    std::atomic<uint64_t> Rotated;
+    std::atomic<uint64_t> RestoreFailed;
+    std::atomic<uint64_t> EpollSources;
+};
+
+extern TStatistics *Statistics;
