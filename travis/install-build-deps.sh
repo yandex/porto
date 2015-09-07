fbe931fcd35cce960cecbf4793057642f659eab9
dreamer.dead <dreamer.dead@gmail.com>
2015-09-07 13:24:54 +0300

    Add missing defines on Travis build.

--- travis/install-build-deps.sh
+++ travis/install-build-deps.sh
@@ -5,6 +5,16 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:canonical-kernel-team/ppa
 
 sudo apt-get update -qq
-sudo apt-get install -qq g++-4.7 flex bison libtool autoconf libncurses5-dev libprotobuf-dev protobuf-compiler linux-headers-goldfish
+sudo apt-get install -qq g++-4.7
+
+# Configuration
+: ${FORCE_SUBREAPER:="1"}
+export FORCE_SUBREAPER
+
+# Trevis build platform doesn't have those newer Linux flags.
+CXXFLAGS="-DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37"
+if [[ "${FORCE_SUBREAPER}" -eq 1 ]]; then
+  export CXXFLAGS
+fi
 
 export CXX="g++-4.7" CC="gcc-4.7"
