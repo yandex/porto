2d3369a894bc1c6a0993a10eb661512f3b8ea8e1
dreamer.dead <dreamer.dead@gmail.com>
2015-09-07 15:53:01 +0300

    Add CAP_BLOCK_SUSPEND flag and get rid of sudo in Travis config.

--- travis/run-build.sh
+++ travis/run-build.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# gcc-4.7
-apt-cache search gcc-4
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y ppa:canonical-kernel-team/ppa
-
-sudo apt-get update -qq
-sudo apt-get install -qq g++-4.7
-
-# Configuration
-: ${FORCE_SUBREAPER:="1"}
-export FORCE_SUBREAPER
+: ${FORCE_NEW_KERNEL_FLAGS:="1"}
+export FORCE_NEW_KERNEL_FLAGS
 
 # Trevis build platform doesn't have those newer Linux flags.
-CXXFLAGS="-DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37"
-if [[ "${FORCE_SUBREAPER}" -eq 1 ]]; then
+CXXFLAGS="-DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37 -DCAP_BLOCK_SUSPEND=36"
+if [[ "${FORCE_NEW_KERNEL_FLAGS}" -eq 1 ]]; then
   export CXXFLAGS
 fi
 
