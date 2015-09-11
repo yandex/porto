f0b58bfc5f5a327bde3d817f2c0d8a48234b15cd
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2015-09-11 14:50:56 +0300

    portod-meta-root: add option --wait <pid>
    When child with that pid exits that app exits too with the same code.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/meta_root.cpp
+++ src/meta_root.cpp
@@ -12,8 +12,10 @@ extern "C" {
 #include <sys/wait.h>
 }
 
+pid_t target = -1;
+
 static void ForwardSignal(int signum) {
-    kill(-1, signum);
+    kill(target, signum);
     ResetSignalHandler(signum);
 }
 
@@ -27,6 +29,15 @@ int main(int argc, char *argv[]) {
             std::cout << GIT_TAG << " " << GIT_REVISION <<std::endl;
             return EXIT_SUCCESS;
         }
+
+        if (arg == "--wait") {
+            argn++;
+            if (argn == argc)
+                return EXIT_FAILURE;
+            target = atoi(argv[argn]);
+            if (kill(target, 0))
+                return EXIT_FAILURE;
+        }
     }
 
     RegisterSignal(SIGTERM, ForwardSignal);
@@ -34,9 +45,29 @@ int main(int argc, char *argv[]) {
     RegisterSignal(SIGQUIT, ForwardSignal);
 
     while (true) {
-        int ret = wait(NULL);
-        if (ret < 0 && errno == ECHILD)
-            sleep(5 * 60);
+        int status;
+        pid_t pid = wait(&status);
+        if (target < 0) {
+            if (pid < 0 && errno == ECHILD)
+                sleep(5 * 60);
+        } else {
+            if (pid < 0) {
+                if (errno == ECHILD)
+                    return 0;
+                if (errno == EINTR)
+                    continue;
+                return EXIT_FAILURE;
+            } else if (pid == target) {
+                if (WIFEXITED(status))
+                    return WEXITSTATUS(status);
+                if (WIFSIGNALED(status)) {
+                    signal(WTERMSIG(status), SIG_DFL);
+                    kill(getpid(), WTERMSIG(status));
+                    return 128 + WTERMSIG(status);
+                }
+                return EXIT_FAILURE;
+            }
+        }
     }
 
     return EXIT_SUCCESS;
