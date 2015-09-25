91078c7353fe7679d03d101889690d06a0e9a76f
dreamer.dead <dreamer.dead@gmail.com>
2015-09-26 00:47:02 +0300

    Add move ctors and operators to TError class.

--- src/api/cpp/error.cpp
+++ src/api/cpp/error.cpp
@@ -8,12 +8,16 @@ extern "C" {
 TError::TError() : Error(EError::Success) {
 }
 
-TError::TError(EError e, std::string description, int eno) :
+TError::TError(EError e, const std::string &description, int eno) :
     Error(e), Description(description), Errno(eno) {
 }
 
-TError::TError(EError e, int eno, std::string description) :
+TError::TError(EError e, int eno, const std::string &description) :
     Error(e), Description(std::string(strerror(eno)) + ": " + description), Errno(eno) {
+}
+
+TError::TError(EError e, std::string &&description, int eno) :
+    Error(e), Description(std::move(description)), Errno(eno) {
 }
 
 TError::operator bool() const {
@@ -42,16 +46,16 @@ TError TError::Serialize(int fd) const {
 
     ret = write(fd, &Error, sizeof(Error));
     if (ret != sizeof(Error))
-        return TError(EError::Unknown, errno, "Can't serialize error");
+        return FromErrno("Can't serialize error");
     ret = write(fd, &Errno, sizeof(Errno));
     if (ret != sizeof(Errno))
-        return TError(EError::Unknown, errno, "Can't serialize errno");
+        return FromErrno("Can't serialize errno");
     ret = write(fd, &len, sizeof(len));
     if (ret != sizeof(len))
-        return TError(EError::Unknown, errno, "Can't serialize length");
+        return FromErrno("Can't serialize length");
     ret = write(fd, Description.data(), len);
     if (ret != len)
-        return TError(EError::Unknown, errno, "Can't serialize description");
+        return FromErrno("Can't serialize description");
 
     return TError::Success();
 }
@@ -59,7 +63,6 @@ TError TError::Serialize(int fd) const {
 bool TError::Deserialize(int fd, TError &error) {
     EError err;
     int errno;
-    std::string desc;
     int ret;
     int len;
 
@@ -67,32 +70,28 @@ bool TError::Deserialize(int fd, TError &error) {
     if (ret == 0)
         return false;
     if (ret != sizeof(Error)) {
-        error = TError(EError::Unknown, errno, "Can't deserialize error");
+        error = FromErrno("Can't deserialize error");
         return true;
     }
     ret = read(fd, &errno, sizeof(Errno));
     if (ret != sizeof(Errno)) {
-        error = TError(EError::Unknown, errno, "Can't deserialize errno");
+        error = FromErrno("Can't deserialize errno");
         return true;
     }
     ret = read(fd, &len, sizeof(len));
     if (ret != sizeof(len)) {
-        error = TError(EError::Unknown, errno, "Can't deserialize length");
+        error = FromErrno("Can't deserialize length");
         return true;
     }
 
-    char *buf = new char[len];
-    ret = read(fd, buf, len);
+    std::string desc(len, '\0');
+    ret = read(fd, &desc[0], len);
     if (ret != len) {
-        delete[] buf;
-        error = TError(EError::Unknown, errno, "Can't deserialize description");
+        error = FromErrno("Can't deserialize description");
         return true;
     }
 
-    desc = std::string(buf, len);
-    delete[] buf;
-
-    error = TError(err, desc, errno);
+    error = TError(err, std::move(desc), errno);
     return true;
 }
 
@@ -104,6 +103,10 @@ const TError& TError::Success() {
 const TError& TError::Queued() {
     static TError e(EError::Queued, "Queued");
     return e;
+}
+
+TError TError::FromErrno(const std::string &description) {
+    return TError(EError::Unknown, errno, description);
 }
 
 std::ostream& operator<<(std::ostream& os, const TError& err) {
--- src/api/cpp/error.hpp
+++ src/api/cpp/error.hpp
@@ -14,11 +14,17 @@ class TError {
 
 public:
     TError();
-    TError(EError e, std::string description, int eno = 0);
-    TError(EError e, int eno, std::string description);
+    TError(EError e, const std::string &description, int eno = 0);
+    TError(EError e, int eno, const std::string &description);
+
+    TError(EError e, std::string &&description, int eno = 0);
+    TError(const TError &other) = default;
+    TError(TError &&other) = default;
+    TError &operator=(TError &&other) = default;
+    TError &operator=(const TError &other) = default;
 
     // return true if non-successful
-    operator bool() const;
+    explicit operator bool() const;
 
     EError GetError() const;
     std::string GetErrorName() const;
@@ -27,8 +33,17 @@ public:
 
     static const TError& Success();
     static const TError& Queued();
+    static TError FromErrno(const std::string &description);
     friend std::ostream& operator<<(std::ostream& os, const TError& err);
 
     TError Serialize(int fd) const;
     static bool Deserialize(int fd, TError &error);
 };
+
+inline bool operator==(const TError &lhs, const TError &rhs) {
+    return lhs.GetError() == rhs.GetError() && lhs.GetErrno() == rhs.GetErrno();
+}
+
+inline bool operator!=(const TError &lhs, const TError &rhs) {
+    return lhs.GetError() != rhs.GetError() || lhs.GetErrno() != rhs.GetErrno();
+}
