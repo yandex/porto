c3b4a83e271ecb077148a96b0634aee2ca5e1c42
Vsevolod Velichko <torkve@yandex-team.ru>
2015-12-22 17:16:13 +0300

    api/python: update package version

--- src/api/python/setup.py
+++ src/api/python/setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='portopy',
-    version='2.6.1',
+    version='2.7.2',
     description='Python API for porto',
     long_description=readme(),
     url='https://github.com/yandex/porto',
