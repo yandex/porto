59bbbe9499cc66b38727cf109d06951cf667e276
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2016-02-12 14:53:59 +0300

    portoinit: rewrite in c
    unlink from util
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/portoinit.c
+++ src/portoinit.c
@@ -1,39 +1,33 @@
-#include <string>
-#include <iostream>
-
-#include "common.hpp"
-#include "version.hpp"
-#include "util/signal.hpp"
-
-extern "C" {
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/prctl.h>
-}
 
-pid_t target = -1;
+#include "version.hpp"
 
-static void ForwardSignal(int signum) {
-    kill(target, signum);
-    ResetSignalHandler(signum);
+static pid_t target = -1;
+
+static void forward(int sig) {
+    kill(target, sig);
+    signal(sig, SIG_DFL);
 }
 
 int main(int argc, char *argv[]) {
     int argn;
 
     for (argn = 1; argn < argc; argn++) {
-        std::string arg(argv[argn]);
-
-        if (arg == "-v" || arg == "--version") {
-            std::cout << GIT_TAG << " " << GIT_REVISION <<std::endl;
+        if (!strcmp(argv[argn], "-v") || !strcmp(argv[argn], "--version")) {
+            printf("%s %s\n", GIT_TAG, GIT_REVISION);
             return EXIT_SUCCESS;
         }
 
-        if (arg == "--wait") {
-            argn++;
-            if (argn == argc)
+        if (!strcmp(argv[argn], "--wait")) {
+            if (++argn == argc)
                 return EXIT_FAILURE;
             target = atoi(argv[argn]);
             if (kill(target, 0))
@@ -42,11 +36,12 @@ int main(int argc, char *argv[]) {
     }
 
     prctl(PR_SET_NAME, "portoinit");
-    RegisterSignal(SIGTERM, ForwardSignal);
-    RegisterSignal(SIGINT, ForwardSignal);
-    RegisterSignal(SIGQUIT, ForwardSignal);
 
-    while (true) {
+    signal(SIGINT, forward);
+    signal(SIGQUIT, forward);
+    signal(SIGTERM, forward);
+
+    while (1) {
         int status;
         pid_t pid = wait(&status);
         if (target < 0) {
