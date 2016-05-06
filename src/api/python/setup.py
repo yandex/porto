a6349f1c7182e054d832074af3d939f155b9e474
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2016-05-06 19:55:11 +0300

    porto: 2.9
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/api/python/setup.py
+++ src/api/python/setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='portopy',
-    version='2.8.8',
+    version='2.9',
     description='Python API for porto',
     long_description=readme(),
     url='https://github.com/yandex/porto',
