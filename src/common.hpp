1eec6f7d9ed77e8f5d0e19239727aef94890512d
Maxim Samoylov <max7255@yandex-team.ru>
2016-06-29 01:53:04 +0300

    porto: replace auto type in common.hpp
    On the gcc with version prior to 4.8, placing
    auto variable into the header, including it and
    not using it in the source file triggers -Wunused-variable.
    
    Let's specify type explicitly to make build output clean.
    
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- src/common.hpp
+++ src/common.hpp
@@ -19,42 +19,42 @@ private:
     TNonCopyable& operator= (TNonCopyable const&&) = delete;
 };
 
-constexpr auto ROOT_TC_MAJOR = 1;
-constexpr auto ROOT_TC_MINOR = 0;
-constexpr auto DEFAULT_TC_MINOR = 2;
+constexpr uint64_t ROOT_TC_MAJOR = 1;
+constexpr uint64_t ROOT_TC_MINOR = 0;
+constexpr uint64_t DEFAULT_TC_MINOR = 2;
 
 /* FIXME add support for 64-bit rate and ceil */
 const uint64_t NET_MAX_LIMIT = UINT32_MAX;
 const uint64_t NET_MAX_GUARANTEE = UINT32_MAX;
-constexpr auto NET_DEFAULT_PRIO = 3;
+constexpr uint64_t NET_DEFAULT_PRIO = 3;
 
-constexpr auto ROOT_CONTAINER_ID = 1;
-constexpr auto PORTO_ROOT_CONTAINER_ID = 3;
+constexpr uint64_t ROOT_CONTAINER_ID = 1;
+constexpr uint64_t PORTO_ROOT_CONTAINER_ID = 3;
 
-constexpr auto ROOT_CONTAINER = "/";
-constexpr auto DOT_CONTAINER = ".";
-constexpr auto SELF_CONTAINER = "self";
-constexpr auto PORTO_ROOT_CONTAINER = "/porto";
+constexpr const char *ROOT_CONTAINER = "/";
+constexpr const char *DOT_CONTAINER = ".";
+constexpr const char *SELF_CONTAINER = "self";
+constexpr const char *PORTO_ROOT_CONTAINER = "/porto";
 
-constexpr auto PORTO_ROOT_CGROUP = "/porto";
-constexpr auto PORTO_DAEMON_CGROUP = "/portod";
+constexpr const char *PORTO_ROOT_CGROUP = "/porto";
+constexpr const char *PORTO_DAEMON_CGROUP = "/portod";
 
-constexpr auto PORTO_GROUP_NAME = "porto";
-constexpr auto PORTO_CONT_GROUP_NAME = "porto-containers";
-constexpr auto CONT_SUFFIX = "-containers";
-constexpr auto PORTO_SOCKET_PATH = "/run/portod.socket";
-constexpr auto PORTO_SOCKET_MODE = 0666;
+constexpr const char *PORTO_GROUP_NAME = "porto";
+constexpr const char *PORTO_CONT_GROUP_NAME = "porto-containers";
+constexpr const char *CONT_SUFFIX = "-containers";
+constexpr const char *PORTO_SOCKET_PATH = "/run/portod.socket";
+constexpr uint64_t PORTO_SOCKET_MODE = 0666;
 
 constexpr int  REAP_EVT_FD = 128;
 constexpr int  REAP_ACK_FD = 129;
 constexpr int  PORTO_SK_FD = 130;
 
-constexpr auto PORTO_VERSION_FILE = "/run/portod.version";
+constexpr const char *PORTO_VERSION_FILE = "/run/portod.version";
 
-constexpr auto CONTAINER_NAME_MAX = 128;
-constexpr auto CONTAINER_PATH_MAX = 200;
-constexpr auto CONTAINER_ID_MAX = 16384;
-constexpr auto CONTAINER_LEVEL_MAX = 7;
-constexpr auto RUN_SUBDIR_LIMIT = 100u;
+constexpr uint64_t CONTAINER_NAME_MAX = 128;
+constexpr uint64_t CONTAINER_PATH_MAX = 200;
+constexpr uint64_t CONTAINER_ID_MAX = 16384;
+constexpr uint64_t CONTAINER_LEVEL_MAX = 7;
+constexpr uint64_t RUN_SUBDIR_LIMIT = 100u;
 
 extern void AckExitStatus(int pid);
