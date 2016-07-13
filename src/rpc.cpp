2c59e07dc075e5b8eaeb93d46f1ad32a655ea65b
Maxim Samoylov <max7255@yandex-team.ru>
2016-07-13 18:02:23 +0300

    porto: fix race on volume layers tmp dir creation
    Multiple clients can manipulate over their volumes
    at the same time, e.g. importing or removing them.
    
    The mechanism behind the process is following:
    the layer to be processed is created/moved as subdir
    in porto_layers/_tmp_ directory then processed and
    either moved back or just deleted.
    
    After all, the porto_layers/_tmp_ directory is also
    removed. So at the start of the process this directory
    should be created.
    
    The race is that if multiple clients will start
    manipulate their volumes, they can collide at the
    _tmp_ directory creation step. Both of them can
    observe that directory doesn't exists and try to
    create them at the same time. One of them will
    fail and abort the user request, which is actuall the
    wrong behaviour.
    
    Do not remove delete porto_layers/_tmp_ after each
    layer manipulation and ensure that the directory exists
    on portod startup.
    
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- src/rpc.cpp
+++ src/rpc.cpp
@@ -1327,11 +1327,7 @@ noinline TError ImportLayer(TContext &context,
     if (!tarball.CanRead(client->GetCred()))
         return TError(EError::Permission, "client has not read access to tarball");
 
-    if (!layers_tmp.Exists()) {
-        error = layers_tmp.Mkdir(0700);
-        if (error)
-            return error;
-    }
+    /* layers_tmp should already be created on startup */
 
     auto vholder_lock = context.Vholder->ScopedLock();
     if (layer.Exists()) {
@@ -1362,16 +1358,12 @@ noinline TError ImportLayer(TContext &context,
         goto err;
 
     error = layer_tmp.Rename(layer);
-    (void)layers_tmp.Rmdir();
-    if (error)
-        goto err;
 
     return TError::Success();
 
 err:
     (void)layer_tmp.RemoveAll();
 err_tmp:
-    (void)layers_tmp.Rmdir();
     return error;
 }
 
