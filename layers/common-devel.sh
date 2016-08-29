9edbc59f5b32fb76d2dd0c99eae46eeef7d53dfc
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2016-08-29 12:21:16 +0300

    layers: add python-protobuf into common-devel.sh
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- layers/common-devel.sh
+++ layers/common-devel.sh
@@ -1,1 +1,5 @@
-apt-get install -y build-essential cmake libc6-dbg strace gdb gdbserver libssl-dev libncurses5-dev
+apt-get install -y build-essential cmake libc6-dbg strace gdb gdbserver
+
+apt-get install -y libssl-dev libncurses5-dev
+
+apt-get install -y python-pip python-protobuf
