aa4d364403b2533f7c2a11f42f0dd8b4888c5edc
Maxim Samoylov <max7255@yandex-team.ru>
2016-12-20 19:51:48 +0300

    test: cleanup volume_places leftovers on failure
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- test/test-volume_places.py
+++ test/test-volume_places.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import tarfile
 import time
-
+import traceback
 import porto
 from test_common import *
 
@@ -29,88 +29,117 @@ except:
 os.mkdir(DIR)
 os.mkdir(PLACE_DIR)
 
-#Prepare dirs and check non-compliant dirs
-assert Catch(c.CreateVolume, path=None, place=PLACE_DIR) == porto.exceptions.InvalidValue
+def Test():
+    #Prepare dirs and check non-compliant dirs
+    assert Catch(c.CreateVolume, path=None, place=PLACE_DIR) == porto.exceptions.InvalidValue
 
-#Prepare dummy layer also
-f = open(DIR + "/file.txt", "w")
-f.write("1234567890")
-f.close()
-t = tarfile.open(name=DIR + "/file_layer.tar", mode="w")
-t.add(DIR + "/file.txt", arcname="file.txt")
-t.close()
+    #Prepare dummy layer also
+    f = open(DIR + "/file.txt", "w")
+    f.write("1234567890")
+    f.close()
+    t = tarfile.open(name=DIR + "/file_layer.tar", mode="w")
+    t.add(DIR + "/file.txt", arcname="file.txt")
+    t.close()
 
 
-os.mkdir(PLACE_DIR + "/porto_volumes")
-assert Catch(c.ImportLayer, "file_layer",
+    os.mkdir(PLACE_DIR + "/porto_volumes")
+    assert Catch(c.ImportLayer, "file_layer",
              DIR + "/file_layer.tar", place=PLACE_DIR) == porto.exceptions.InvalidValue
-assert Catch(c.CreateVolume, path=None,
-             place=PLACE_DIR) == porto.exceptions.InvalidValue
-os.mkdir(PLACE_DIR + "/porto_layers")
-os.mkdir(DIR + "/a")
+    assert Catch(c.CreateVolume, path=None,
+                 place=PLACE_DIR) == porto.exceptions.InvalidValue
+    os.mkdir(PLACE_DIR + "/porto_layers")
+    os.mkdir(DIR + "/a")
 
-#Finally, checking functions
-assert Catch(c.CreateVolume, path=DIR + "/a", place=PLACE_DIR, backend="native", layers=["ubuntu-precise"]) == porto.exceptions.LayerNotFound
+    #Finally, checking functions
+    assert Catch(c.CreateVolume, path=DIR + "/a", place=PLACE_DIR, backend="native", layers=["ubuntu-precise"]) == porto.exceptions.LayerNotFound
 
-v = c.CreateVolume(path=None, layers=["ubuntu-precise"], backend="plain")
-v.Export(DIR + "/tmp_ubuntu_precise.tar")
-c.ImportLayer("place-ubuntu-precise", DIR + "/tmp_ubuntu_precise.tar", place=PLACE_DIR)
-assert Catch(c.FindLayer, "place-ubuntu-precise") == porto.exceptions.LayerNotFound
+    v = c.CreateVolume(path=None, layers=["ubuntu-precise"], backend="plain")
+    v.Export(DIR + "/tmp_ubuntu_precise.tar")
+    c.ImportLayer("place-ubuntu-precise", DIR + "/tmp_ubuntu_precise.tar", place=PLACE_DIR)
+    assert Catch(c.FindLayer, "place-ubuntu-precise") == porto.exceptions.LayerNotFound
 
-l = c.FindLayer("place-ubuntu-precise", place=PLACE_DIR)
+    l = c.FindLayer("place-ubuntu-precise", place=PLACE_DIR)
 
-assert l.GetPrivate() == ""
-l.SetPrivate("XXXX")
-assert l.GetPrivate() == "XXXX"
+    assert l.GetPrivate() == ""
+    l.SetPrivate("XXXX")
+    assert l.GetPrivate() == "XXXX"
 
-l.Merge(DIR + "/file_layer.tar", private_value="YYYY")
-assert l.GetPrivate() == "YYYY"
+    l.Merge(DIR + "/file_layer.tar", private_value="YYYY")
+    assert l.GetPrivate() == "YYYY"
 
-os.unlink(DIR + "/tmp_ubuntu_precise.tar")
-v.Unlink("/")
+    os.unlink(DIR + "/tmp_ubuntu_precise.tar")
+    v.Unlink("/")
 
-#Should also fail because of foreign layer vise versa
-assert Catch(c.CreateVolume, path=DIR + "/a", backend="native", layers=["place-ubuntu-precise"]) == porto.exceptions.LayerNotFound
+    #Should also fail because of foreign layer vise versa
+    assert Catch(c.CreateVolume, path=DIR + "/a", backend="native", layers=["place-ubuntu-precise"]) == porto.exceptions.LayerNotFound
 
-#Check volume is working properly
-v = c.CreateVolume(path=DIR + "/a", place=PLACE_DIR, backend="native", layers=["place-ubuntu-precise"])
+    #Check volume is working properly
+    v = c.CreateVolume(path=DIR + "/a", place=PLACE_DIR, backend="native", layers=["place-ubuntu-precise"])
 
-place_volumes = os.listdir(PLACE_DIR + "/porto_volumes")
-assert len(place_volumes) == 1
+    place_volumes = os.listdir(PLACE_DIR + "/porto_volumes")
+    assert len(place_volumes) == 1
 
-cont = c.Create("test")
-cont.SetProperty("command", "bash -c \"echo -n 789987 > /123321.txt\"")
-cont.SetProperty("root", v.path)
-cont.Start()
-cont.Wait()
-cont.Stop()
+    cont = c.Create("test")
+    cont.SetProperty("command", "bash -c \"echo -n 789987 > /123321.txt\"")
+    cont.SetProperty("root", v.path)
+    cont.Start()
+    cont.Wait()
+    cont.Stop()
 
-f = open(PLACE_DIR + "/porto_volumes/" + place_volumes[0] + "/native/123321.txt", "r")
-assert f.read() == "789987"
+    f = open(PLACE_DIR + "/porto_volumes/" + place_volumes[0] + "/native/123321.txt", "r")
+    assert f.read() == "789987"
 
-cont.SetProperty("command", "cat /file.txt")
-cont.Start()
-cont.Wait()
-assert cont.Get(["stdout"])["stdout"] == "1234567890"
-cont.Destroy()
+    cont.SetProperty("command", "cat /file.txt")
+    cont.Start()
+    cont.Wait()
+    assert cont.Get(["stdout"])["stdout"] == "1234567890"
+    cont.Destroy()
 
-#Check what will be if we rename our place
+    #Check what will be if we rename our place
 
-assert len(c.ListLayers(place=PLACE_DIR)) == 1
-os.renames(PLACE_DIR, PLACE_DIR+"1")
-assert Catch(c.ListLayers, place=PLACE_DIR) == porto.exceptions.UnknownError
-os.renames(PLACE_DIR + "1", PLACE_DIR)
-assert len(c.ListLayers(place=PLACE_DIR)) == 1
+    assert len(c.ListLayers(place=PLACE_DIR)) == 1
+    os.renames(PLACE_DIR, PLACE_DIR+"1")
+    assert Catch(c.ListLayers, place=PLACE_DIR) == porto.exceptions.UnknownError
+    os.renames(PLACE_DIR + "1", PLACE_DIR)
+    assert len(c.ListLayers(place=PLACE_DIR)) == 1
 
-v.Export(DIR + "/tmp_back_ubuntu_precise.tar")
+    v.Export(DIR + "/tmp_back_ubuntu_precise.tar")
 
-v.Unlink()
+    v.Unlink()
 
-c.RemoveLayer("place-ubuntu-precise", place=PLACE_DIR)
+    c.RemoveLayer("place-ubuntu-precise", place=PLACE_DIR)
 
-assert len(os.listdir(PLACE_DIR + "/porto_volumes")) == 0
-assert len(c.ListLayers(place=PLACE_DIR)) == 0
-assert len(os.listdir(PLACE_DIR + "/porto_layers")) == 0
+    assert len(os.listdir(PLACE_DIR + "/porto_volumes")) == 0
+    assert len(c.ListLayers(place=PLACE_DIR)) == 0
+    assert len(os.listdir(PLACE_DIR + "/porto_layers")) == 0
+
+
+ret = 0
+
+try:
+    Test()
+except BaseException as e:
+    print traceback.format_exc()
+    ret = 1
+
+for r in c.ListContainers():
+    try:
+        r.Destroy()
+    except:
+        pass
+
+for v in c.ListVolumes():
+    try:
+        v.Unlink()
+    except:
+        pass
+
+try:
+    c.RemoveLayer("place-ubuntu-precise", place=PLACE_DIR)
+except:
+    pass
 
 shutil.rmtree(DIR)
 shutil.rmtree(PLACE_DIR)
+
+sys.exit(ret)
