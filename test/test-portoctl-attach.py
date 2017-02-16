547d963a9038b2642efab20153ca3d419b605799
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2017-02-16 11:51:34 +0300

    test/test-portoctl-attach.py: fix default porto namespace
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-portoctl-attach.py
+++ test/test-portoctl-attach.py
@@ -1,5 +1,12 @@
 import subprocess
 from test_common import *
 
-assert subprocess.check_output([portoctl, 'exec', 'test', 'command=bash -c "' + portoctl + ' create test/test; ' + portoctl + ' set test/test isolate false; ' + portoctl +' start test/test; bash -c \\"' + portoctl + ' get self absolute_name; ' + portoctl + ' attach test/test \\\$\\\$; ' + portoctl + ' get self absolute_name\\""'],
-                               stdin=subprocess.PIPE, stderr=subprocess.PIPE) == '/porto/test\n/porto/test/test\n'
+output = subprocess.check_output([portoctl, 'exec', 'test',
+                               'command=bash -c "' + portoctl + ' create self/test; ' +
+                               portoctl + ' set self/test isolate false; ' +
+                               portoctl + ' start self/test; bash -c \\"' +
+                               portoctl + ' get self absolute_name; ' +
+                               portoctl + ' attach self/test \\\$\\\$; ' +
+                               portoctl + ' get self absolute_name\\""'],
+                               stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+assert output == '/porto/test\n/porto/test/test\n', "unexpected output: " + output
