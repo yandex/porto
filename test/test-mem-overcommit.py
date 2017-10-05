90e1eaa4fe7f5f80799fd863e280ec0f78619e63
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2017-10-05 19:22:25 +0300

    test: fix mem-overcommit
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-mem-overcommit.py
+++ test/test-mem-overcommit.py
@@ -21,44 +21,55 @@ if "memory_guarantee" not in c.Plist():
     print "SKIP memory_guarantee"
     sys.exit()
 
-r = c.Create("test")
-r = c.Create("testa")
-r = c.Create("test/test1")
-r = c.Create("test/test2")
-r = c.Create("test/test3")
-r = c.Create("test/testa")
-r = c.Find("test")
-r.SetProperty("memory_guarantee", "128")
-assert r.GetProperty("memory_guarantee") == "128"
-r.SetProperty("memory_guarantee", "131072")
-assert r.GetProperty("memory_guarantee") == "131072"
-r.SetProperty("memory_guarantee", "1G")
-assert r.GetProperty("memory_guarantee") == "1073741824"
-r.SetProperty("memory_guarantee", "0")
-assert r.GetProperty("memory_guarantee") == "0"
-r.SetProperty("memory_guarantee", "256000000")
-assert r.GetProperty("memory_guarantee") == "256000000"
-r = c.Find("test/test1")
-r.SetProperty("memory_guarantee", "384000000")
-r = c.Find("test/test2")
-r.SetProperty("memory_guarantee", "384000000")
-r = c.Find("test/test3")
-r.SetProperty("memory_guarantee", "384000000")
-r = c.Find("test/testa")
-assert Catch(r.SetProperty, "memory_guarantee", "1000G") == porto.exceptions.ResourceNotAvailable
-r = c.Find("testa")
-r.SetProperty("memory_guarantee", "3.0G")
-assert r.GetProperty("memory_guarantee") == "3221225472"
-assert Catch(r.SetProperty, "memory_guarantee", "1000G") == porto.exceptions.ResourceNotAvailable
-assert r.GetProperty("memory_guarantee") == "3221225472"
-r.SetProperty("memory_guarantee", "0")
-assert r.GetProperty("memory_guarantee") == "0"
-c.Destroy("test/testa")
-c.Destroy("test/test3")
-c.Destroy("test/test2")
-c.Destroy("test/test1")
-c.Destroy("testa")
-c.Destroy("test")
+ct0 = c.Create("test")
+ct1 = c.Create("test/test1")
+ct2 = c.Create("test/test2")
+ct3 = c.Create("test/test3")
+ct4 = c.Create("test/test4")
+
+ct0.SetProperty("memory_guarantee", "128")
+assert ct0.GetProperty("memory_guarantee") == "128"
+ct0.SetProperty("memory_guarantee", "131072")
+assert ct0.GetProperty("memory_guarantee") == "131072"
+ct0.SetProperty("memory_guarantee", "1G")
+assert ct0.GetProperty("memory_guarantee") == "1073741824"
+ct0.SetProperty("memory_guarantee", "0")
+assert ct0.GetProperty("memory_guarantee") == "0"
+ct0.SetProperty("memory_guarantee", "256000000")
+assert ct0.GetProperty("memory_guarantee") == "256000000"
+ct0.SetProperty("memory_guarantee", "1T")
+assert ct0.GetProperty("memory_guarantee") == "1099511627776"
+
+assert Catch(ct0.Start) == porto.exceptions.ResourceNotAvailable
+
+ct0.SetProperty("memory_guarantee", "0")
+assert ct0.GetProperty("memory_guarantee") == "0"
+
+ct0.Start()
+
+assert Catch(ct0.SetProperty, "memory_guarantee", "1T") == porto.exceptions.ResourceNotAvailable
+
+ct1.SetProperty("memory_guarantee", "256M")
+ct2.SetProperty("memory_guarantee", "256M")
+ct3.SetProperty("memory_guarantee", "256M")
+ct4.SetProperty("memory_guarantee", "256M")
+
+ct1.Start()
+ct2.Start()
+ct3.Start()
+ct4.Start()
+
+Catch(ct4.SetProperty, "memory_guarantee", "1T")
+
+assert Catch(ct4.SetProperty, "memory_guarantee", "1T") == porto.exceptions.ResourceNotAvailable
+ct4.SetProperty("memory_guarantee", "3.0G")
+assert ct4.GetProperty("memory_guarantee") == "3221225472"
+assert Catch(ct4.SetProperty, "memory_guarantee", "1T") == porto.exceptions.ResourceNotAvailable
+assert ct4.GetProperty("memory_guarantee") == "3221225472"
+ct4.SetProperty("memory_guarantee", "0")
+assert ct4.GetProperty("memory_guarantee") == "0"
+
+ct0.Destroy()
 
 r = c.Create("test1")
 r.SetProperty("memory_guarantee", "1M")
@@ -92,6 +103,7 @@ r.SetProperty("memory_guarantee", "3M")
 assert r.GetProperty("memory_guarantee") == "3145728"
 
 r = c.Find("test2")
+r.Start()
 r.SetProperty("memory_guarantee", "2M")
 assert r.GetProperty("memory_guarantee") == "2097152"
 r.SetProperty("memory_guarantee", "2.6M")
