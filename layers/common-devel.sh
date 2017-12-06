85e155239849a720f3a70540b02ac0e0f31f741a
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2017-12-06 11:01:29 +0300

    layers: fix pip
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- layers/common-devel.sh
+++ layers/common-devel.sh
@@ -2,8 +2,8 @@ apt-get install -y build-essential cmake libc6-dbg strace gdb gdbserver
 
 apt-get install -y libncurses5-dev
 
+apt-get install -y libprotobuf-dev protobuf-compiler
+
 apt-get install -y python-pip
 
-apt-get install -y protobuf-compiler
-
-pip install protobuf
+pip install --index-url=https://pypi.python.org/simple protobuf
