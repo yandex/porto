36c8a648128c6bb490650a92ea273b96a1fe88c3
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-02-19 14:22:08 +0300

    python: create exceptions for all errors
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/api/python/porto/exceptions.py
+++ src/api/python/porto/exceptions.py
@@ -1,32 +1,15 @@
 from . import rpc_pb2
 
-
-class SocketError(Exception):
-    pass
-
-
-class SocketTimeout(Exception):
-    pass
-
-
-class UnknownError(Exception):
-    def __str__(self):
-        return '{}: {}'.format(self.__class__.__name__, self.message)
-
-
 class EError(Exception):
     EID = None
     __TYPES__ = {}
 
-    class __metaclass__(type):
-        def __new__(mcs, cls, bases, dct):
-            new_class = type.__new__(mcs, cls, bases, dct)
-
-            eid = dct.get('EID')
-            if eid is not None:
-                EError.__TYPES__[eid] = new_class
-
-            return new_class
+    @classmethod
+    def _Init(cls):
+        for name, eid in rpc_pb2.EError.items():
+            e_class = type(name, (cls,), {'EID': eid})
+            cls.__TYPES__[eid] = e_class
+            globals()[name] = e_class
 
     @classmethod
     def Create(cls, eid, msg):
@@ -39,77 +22,16 @@ class EError(Exception):
         return '{}: {}'.format(self.__class__.__name__, self.message)
 
 
-class InvalidMethod(EError):
-    EID = rpc_pb2.InvalidMethod
+class SocketError(EError):
+    pass
 
 
-class ContainerAlreadyExists(EError):
-    EID = rpc_pb2.ContainerAlreadyExists
+class SocketTimeout(EError):
+    pass
 
 
-class ContainerDoesNotExist(EError):
-    EID = rpc_pb2.ContainerDoesNotExist
+class UnknownError(EError):
+    pass
 
 
-class InvalidProperty(EError):
-    EID = rpc_pb2.InvalidProperty
-
-
-class InvalidData(EError):
-    EID = rpc_pb2.InvalidData
-
-
-class InvalidValue(EError):
-    EID = rpc_pb2.InvalidValue
-
-
-class InvalidState(EError):
-    EID = rpc_pb2.InvalidState
-
-
-class NotSupported(EError):
-    EID = rpc_pb2.NotSupported
-
-
-class ResourceNotAvailable(EError):
-    EID = rpc_pb2.ResourceNotAvailable
-
-
-class PermissionError(EError):
-    EID = rpc_pb2.Permission
-
-
-class VolumeAlreadyExists(EError):
-    EID = rpc_pb2.VolumeAlreadyExists
-
-
-class VolumeNotFound(EError):
-    EID = rpc_pb2.VolumeNotFound
-
-
-class VolumeAlreadyLinked(EError):
-    EID = rpc_pb2.VolumeAlreadyLinked
-
-
-class VolumeNotLinked(EError):
-    EID = rpc_pb2.VolumeNotLinked
-
-
-class VolumeNotReady(EError):
-    EID = rpc_pb2.VolumeNotReady
-
-
-class LayerAlreadyExists(EError):
-    EID = rpc_pb2.LayerAlreadyExists
-
-
-class LayerNotFound(EError):
-    EID = rpc_pb2.LayerNotFound
-
-
-class NoSpace(EError):
-    EID = rpc_pb2.NoSpace
-
-
-class Busy(EError):
-    EID = rpc_pb2.Busy
+EError._Init()
