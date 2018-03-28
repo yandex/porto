67fd47888c1468d7cf3171918b5c7d5f52f1881a
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-03-28 19:59:43 +0300

    python/api: recompile rpc.proto at setup
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/api/python/setup.py
+++ src/api/python/setup.py
@@ -1,29 +1,46 @@
 from setuptools import setup
 import re
 import os
+import sys
 
 def version():
-    if os.path.exists('PKG-INFO'):
+    if os.path.exists("PKG-INFO"):
         with open("PKG-INFO") as f:
             return re.search(r'\nVersion: (\S+)\n', f.read()).group(1)
-    with open("../../../debian/changelog") as f:
-        return re.search(r'.*\((.*)\).*', f.readline()).group(1)
+    if os.path.exists("../../../debian/changelog"):
+        with open("../../../debian/changelog") as f:
+            return re.search(r'.*\((.*)\).*', f.readline()).group(1)
+    return "0.0.0"
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='portopy',
-    version=version(),
-    description='Python API for porto',
-    long_description=readme(),
-    url='https://github.com/yandex/porto',
-    author='marchael',
-    author_email='marchael@yandex-team.ru',
-    license='none',
-    packages=['porto'],
-    install_requires=[
-        'protobuf',
-    ],
-    zip_safe=False)
+if __name__ == '__main__':
 
+    if not os.path.exists("porto/rpc.proto"):
+        with open("../../rpc.proto", 'r') as src, open("porto/rpc.proto", 'w') as dst:
+            dst.write(src.read())
+
+    try:
+        import subprocess
+        subprocess.check_call(['protoc', '--python_out=porto', '--proto_path=porto', 'porto/rpc.proto'])
+    except Exception as e:
+        sys.stderr.write("Cannot compile rpc.proto: {}\n".format(e))
+
+    if not os.path.exists("porto/rpc_pb2.py"):
+        sys.stderr.write("Compiled rpc.proto not found\n")
+        sys.exit(-1)
+
+    setup(name='portopy',
+          version=version(),
+          description='Python API for porto',
+          long_description=readme(),
+          url='https://github.com/yandex/porto',
+          author_email='marchael@yandex-team.ru',
+          maintainer_email='khlebnikov@yandex-team.ru',
+          license='GNU LGPL v3 License',
+          packages=['porto'],
+          package_data={'porto': ['rpc.proto']},
+          install_requires=['protobuf'],
+          zip_safe=False)
