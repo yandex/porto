1e141a87cf61cd05d9a74c4805e9ee06fab2ae64
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-04-17 12:04:30 +0300

    test/tc-rebuild: get network interfaces from sysfs
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-tc-rebuild.py
+++ test/test-tc-rebuild.py
@@ -3,28 +3,25 @@ import subprocess
 import test_common
 from test_common import *
 import re
+import os
 
 conn = porto.Connection()
 
 expected_errors = int(conn.GetData('/', 'porto_stat[errors]'))
 expected_warnings = int(conn.GetData('/', 'porto_stat[warnings]'))
 
-# get managed links
-links = [l.split(':')[0].strip() for l in conn.GetData('/', 'net_bytes').split(';') if l]
-links = [l for l in links if not l.startswith('group ')]
-assert links
-
-def check_qdisc(link):
+def has_qdisc(link):
     return re.match('qdisc (htb|hfsc) ', subprocess.check_output(['tc', 'qdisc', 'show', 'dev', link]))
 
 def del_qdisc(link):
     subprocess.check_call(['tc', 'qdisc', 'del', 'root', 'dev', link])
 
-# check and delete qdisc
-for link in links:
-    assert check_qdisc(link)
+managed_links = [link for link in os.listdir('/sys/class/net') if has_qdisc(link)]
+assert managed_links
+
+for link in managed_links:
     del_qdisc(link)
-    assert not check_qdisc(link)
+    assert not has_qdisc(link)
 
 # start test container
 test = conn.Create('test')
@@ -35,8 +32,8 @@ assert test.GetData('exit_status') == '0'
 test.Destroy()
 
 # recheck qdisc
-for link in links:
-    assert check_qdisc(link)
+for link in managed_links:
+    assert has_qdisc(link)
 
 ExpectEq(int(conn.GetData('/', 'porto_stat[errors]')), expected_errors)
 ExpectEq(int(conn.GetData('/', 'porto_stat[warnings]')), expected_warnings)
