2788a254d0699ab8d405e6170211d2c3039ef883
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-09-04 08:32:36 +0300

    network: fix L3 migration/sharing
    Commit 36ff86f4e5588de043191b19239021d7b9e1b700 upstream.
    
    Do not share net=none and consider only running containers.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-net.py
+++ test/test-net.py
@@ -3,6 +3,7 @@ import porto
 
 conn = porto.Connection()
 
+ExpectEq(conn.GetProperty("/", "porto_stat[networks]"), "1")
 
 # inherit over isolate=false
 a = conn.Run("a", net="none")
@@ -29,5 +30,13 @@ a = conn.Run("a", net="L3 veth", ip="veth 198.51.100.0", default_gw="veth 198.51
 ExpectEq(conn.GetProperty("/", "porto_stat[networks]"), "2")
 b = conn.Run("b", net="L3 veth", ip="veth 198.51.100.0", default_gw="veth 198.51.100.1")
 ExpectEq(conn.GetProperty("/", "porto_stat[networks]"), "2")
+a.Destroy()
+b.Destroy()
+
+
+# net=none
+a = conn.Run("a", net="none", command="sleep 60")
+b = conn.Run("b", net="none", command="sleep 60")
+ExpectEq(conn.GetProperty("/", "porto_stat[networks]"), "3")
 a.Destroy()
 b.Destroy()
