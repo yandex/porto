efd58271df8d1c800637894026e7d27e8646353a
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-09-13 16:05:26 +0300

    porto: disable default top-level memory_limit
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/config.cpp
+++ src/config.cpp
@@ -99,7 +99,7 @@ static void DefaultConfig() {
 
     config().mutable_container()->set_min_memory_limit(1ull << 20); /* 1Mb */
 
-    config().mutable_container()->set_memory_limit_margin(2ull << 30); /* 2Gb */
+    // config().mutable_container()->set_memory_limit_margin(2ull << 30); /* 2Gb */
 
     config().mutable_container()->set_anon_limit_margin(16ull << 20); /* 16Mb */
 
