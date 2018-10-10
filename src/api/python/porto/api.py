5efc64ca6b8d122b5ac0f4074ab3d92dcdf88b5b
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-10-10 19:12:18 +0300

    test: add legacy-root-loop
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/api/python/porto/api.py
+++ src/api/python/porto/api.py
@@ -708,7 +708,7 @@ class Connection(object):
         except exceptions.PortoException as e:
             try:
                 ct.Destroy()
-            except exceptions.PortoException.ContainerDoesNotExist:
+            except exceptions.ContainerDoesNotExist:
                 pass
             raise e
         return ct
