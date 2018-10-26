0a6cfc678da39e71b272228b1866972b40d2b27c
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-10-26 10:45:02 +0300

    test/wait: check nonblock and timeout
    Commit e78ff8a21299da311e92e70b4560cbe196fe9387 upstream.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-wait.py
+++ test/test-wait.py
@@ -3,25 +3,40 @@ import porto
 
 c = porto.Connection()
 
+# stopped
 a = c.Create("a")
 ExpectEq(c.WaitContainers(["a"]), "a")
 a.Destroy()
 
+# dead
 a = c.Run("a", command="true")
 ExpectEq(c.WaitContainers(["a"]), "a")
 a.Destroy()
 
+# non-block
+a = c.Run("a", command="sleep 1000")
+ExpectEq(Catch(c.WaitContainers, ["a"], timeout=0), porto.exceptions.WaitContainerTimeout)
+a.Destroy()
+
+# timeout
+a = c.Run("a", command="sleep 1000")
+ExpectEq(Catch(c.WaitContainers, ["a"], timeout=0.1), porto.exceptions.WaitContainerTimeout)
+a.Destroy()
+
+# setup async
 events = []
 def wait_event(name, state, when):
     ExpectEq((name, state), events.pop(0))
 
 c.AsyncWait(["a"], wait_event)
 
+# full async cycle
 events=[('a', 'stopped'), ('a', 'starting'), ('a', 'running'), ('a', 'stopping'), ('a', 'stopped'), ('a', 'destroyed')]
 a = c.Run("a", command="true")
 a.Destroy()
 ExpectEq(events, [])
 
+# restore async at reconnect
 events=[('a', 'stopped'), ('a', 'starting'), ('a', 'running'), ('a', 'running'), ('a', 'stopping'), ('a', 'stopped'), ('a', 'destroyed')]
 a = c.Run("a", weak=False, command="sleep 1000")
 ReloadPortod()
