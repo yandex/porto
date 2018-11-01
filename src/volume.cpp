a95e7989c9f44a5176c57a3916c4ec26e0760877
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-11-01 10:00:53 +0300

    volumes: fix leak in place_usage accounting
    Commit 55be54a19f6799e5eb6c627b5bc1cd9611807f89 upstream.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- src/volume.cpp
+++ src/volume.cpp
@@ -128,10 +128,6 @@ public:
                 Volume->GetMountFlags() | MS_SLAVE | MS_SHARED);
     }
 
-    std::string ClaimPlace() override {
-        return "";
-    }
-
     TError Destroy() override {
         return Volume->InternalPath.UmountAll();
     }
@@ -1613,10 +1609,11 @@ TError TVolume::ClaimPlace(uint64_t size) {
     if (place == "")
         return OK;
 
-    if ((!size || size > ClaimedSpace) && !CL->IsInternalUser() &&
-            State != EVolumeState::Destroying) {
-        for (auto ct = VolumeOwnerContainer; ct; ct = ct->Parent) {
-            ct->LockStateWrite();
+    for (auto ct = VolumeOwnerContainer; ct; ct = ct->Parent) {
+        ct->LockStateWrite();
+
+        if ((!size || size > ClaimedSpace) && !CL->IsInternalUser() &&
+                State != EVolumeState::Destroying) {
 
             uint64_t total_limit = ct->PlaceLimit.count("total") ?
                                    ct->PlaceLimit.at("total") : UINT64_MAX;
@@ -1646,14 +1643,18 @@ TError TVolume::ClaimPlace(uint64_t size) {
                     c->UnlockState();
                 }
 
-                return TError(EError::ResourceNotAvailable, "Not enough place limit in {}", ct->Name);
+                return TError(EError::ResourceNotAvailable,
+                              "Not enough place limit in {} for {} limit {}, total usage {} of {}, {} usage {} of {}",
+                              ct->Name, Path, StringFormatSize(size - ClaimedSpace),
+                              StringFormatSize(total_usage), StringFormatSize(total_limit),
+                              place, StringFormatSize(place_usage), StringFormatSize(place_limit));
             }
-
-            ct->PlaceUsage["total"] += size - ClaimedSpace;
-            ct->PlaceUsage[place] += size - ClaimedSpace;
-
-            ct->UnlockState();
         }
+
+        ct->PlaceUsage["total"] += size - ClaimedSpace;
+        ct->PlaceUsage[place] += size - ClaimedSpace;
+
+        ct->UnlockState();
     }
 
     auto volumes_lock = LockVolumes();
