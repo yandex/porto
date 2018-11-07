09f69501311799dd5a941b80f84a01e42454dd16
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2018-11-07 13:12:13 +0300

    porto: fix cgroup removal on error path of virt_mode=job
    Commit 3f311dad99f87d613e5e665278a9eecbe224dde2 upstream.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

--- test/test-jobs.py
+++ test/test-jobs.py
@@ -3,17 +3,44 @@ from test_common import *
 
 c = porto.Connection(timeout=30)
 
-a = c.Run('a', weak=False, command='sleep 1000')
+a = c.Run('test-a', weak=False, command='sleep 1000')
 ExpectEq(a['state'], 'running')
-b = c.Run('a/b', weak=False, virt_mode='job', command='sleep 1000')
+
+ExpectEq(Catch(c.Run, 'test-a/b', virt_mode='job', memory_limit="1G"), porto.exceptions.InvalidValue)
+
+b = c.Run('test-a/b', weak=False, virt_mode='job', command='sleep 1000')
 ExpectEq(b['state'], 'running')
+
 ExpectEq(a['cgroups'], b['cgroups'])
+
 ReloadPortod()
+
 ExpectEq(b['state'], 'running')
+
 b.Kill(9)
 b.Wait()
 ExpectEq(b['state'], 'dead')
 ExpectEq(b['exit_code'], '-9')
+
 b.Destroy()
+
 ExpectEq(a['state'], 'running')
+
+a.Destroy()
+
+
+# host/job
+
+a = c.Run('test-a', virt_mode='host', memory_limit="1G")
+ExpectEq(a['state'], 'meta')
+
+b = c.Run('test-a/b', virt_mode='job', command='sleep 1000')
+ExpectEq(b['state'], 'running')
+b.Destroy()
+
+b = c.Run('test-a/b', virt_mode='job', command='sleep 1000')
+ExpectEq(b['state'], 'running')
+b.Destroy()
+
+ExpectEq(a['state'], 'meta')
 a.Destroy()
