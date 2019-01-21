e638b7c5f73544a6d6abeed793b71793923357c8
Maxim Samoylov <max7255@gmail.com>
2019-01-21 21:52:10 +0000

    test/net-sched: test both schedulers, fix htb limiting, improve assertions
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- test/test-net-sched.py
+++ test/test-net-sched.py
@@ -1,24 +1,18 @@
 import porto
 import json
 import time
+import os
+import sys
 from test_common import *
 
+remote_host = os.environ.get('PORTO_IPERF3_SERVER', None)
 
-server1=("kernel1.search.yandex.net", 5201)
-server2=("kernel1.search.yandex.net", 5202)
-server3=("kernel1.search.yandex.net", 5203)
+if not remote_host:
+    print "No remote host specified, skipping test"
+    sys.exit(0)
 
-
-conn = porto.Connection()
-
-
-print "Setup uplink limit 10M"
-ConfigurePortod('test-net-sched', """
-network {
-    device_ceil: "default: 10M"
-}
-""")
-
+server1=(remote_host, 5201)
+server2=(remote_host, 5202)
 
 
 def run_iperf(name, server, wait=None, cs=None, cfg={}, **kwargs):
@@ -46,40 +40,98 @@ def bps(ct):
     return res['end']['sum_sent']['bits_per_second'] / 2.**23
 
 
-a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_limit": "default: 0"})
-res = bps(a)
-print "net_limit inf -> ", res
-ExpectRange(res, 8, 12)
+def run_test(rate=10):
+    a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_limit": "default: 0"})
+    res = bps(a)
+    print "net_limit inf -> ", res
+    ExpectRange(res, rate * 0.8, rate * 1.33)
 
-a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_limit": "default: 1M"})
-res = bps(a)
-print "net_limit 1M -> ", res
-ExpectRange(res, 0.5, 2)
+    a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_limit": "default: %sM" % int(rate * 0.1)})
+    res = bps(a)
+    print "net_limit %sM -> " % int(rate * 0.1), res
+    ExpectRange(res, rate * 0.05, rate * 0.2)
 
+    b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={})
+    a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={})
+    res = bps(a)
+    res_b = bps(b)
 
-b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={})
-a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={})
-res = bps(a)
-res_b = bps(b)
-print "net_guarantee 0 and 0 -> ", res, res_b
-ExpectRange(res, 4, 6)
+    b_rate = (5 * res_b - 2 * rate) / 3
+    print "net_guarantee 0 and 0 -> %s and %s (%s measured)" % (res, b_rate, res_b)
 
+    # Thresholds rationale:
+    # We expect "even" distribution during parallel sending phase, also:
+    # 1) Allow up to +- 50% fairness between containers, mind the default rate of 10M
+    # 2) Use relative estimation for b container
 
-b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={"net_guarantee": "default: 1M"})
-a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_guarantee": "default: 1M"})
-res = bps(a)
-res_b = bps(b)
-print "net_guarantee 1M and 1M -> ", res, res_b
-ExpectRange(res, 4, 6)
+    ExpectRange(res / b_rate, 0.5, 1.5)
 
+    b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={"net_guarantee": "default: %sM" % int(rate * 0.1)})
+    a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_guarantee": "default: %sM" % int(rate * 0.1)})
+    res = bps(a)
+    res_b = bps(b)
+    b_rate = (5 * res_b - 2 * rate) / 3
+    print "net_guarantee %sM and %sM -> %s and %s (%s measured)" %(int(rate * 0.1), int(rate * 0.1), res, b_rate, res_b)
 
-b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={"net_guarantee": "default: 1M"})
-a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_guarantee": "default: 9M"})
-res = bps(a)
-res_b = bps(b)
-print "net_guarantee 9M and 1M -> ", res, res_b
-ExpectRange(res, 8, 10)
+    # We demand guarantee and also expect "even distribution"
+    # 1) no less than -10% from guarantee, 0.09
+    # 2) Use relative estimation for b container
+    # 3) Allow up to +- 50% fairness between containers, mind the default rate of 10M
 
+    ExpectLe(0.09, res / rate)
+    ExpectLe(0.09, res_b / rate)
+    ExpectRange(res / b_rate, 0.5, 1.5)
 
-print "Remove uplink limit"
-ConfigurePortod('test-net-sched', "")
+    b = run_iperf("test-net-b", server2, time=5, wait=0, cfg={"net_guarantee": "default: %sM" % int(rate * 0.1)})
+    a = run_iperf("test-net-a", server1, time=3, wait=5, cfg={"net_guarantee": "default: %sM" % int(rate * 0.9)})
+    res = bps(a)
+    res_b = bps(b)
+    b_rate = (5 * res_b - 2 * rate) / 3
+    print "net_guarantee %sM and %sM -> %s and %s measured (%s b_rate)" % (int(rate * 0.9), int(rate * 0.1), res, res_b, b_rate)
+
+    # We demand guarantees to be followed
+    # 1) no less than -10% from guarantee, 0.8
+    # 2) net-b guarantee keeped
+
+    ExpectLe(0.85, res / rate)
+    ExpectLe(0.09, res_b / rate)
+
+conn = porto.Connection()
+rate = 10
+
+try:
+    print "Test HTB scheduler"
+    print "Setup uplink limit %sM" % rate
+
+    ConfigurePortod('test-net-sched', """
+network {
+    device_ceil: "default: %sM
+    device_rate: "default: %sM"
+    device_qdisc: "default: htb"
+}
+""" % (rate, rate))
+
+    run_test(rate)
+
+    print "Test HFSC scheduler"
+    print "Setup uplink limit %sM" % rate
+
+    ConfigurePortod('test-net-sched', """
+network {
+    device_ceil: "default: %sM"
+    device_qdisc: "default: hfsc"
+}
+""" % rate)
+
+    run_test(rate)
+
+finally:
+    cts = conn.List()
+    if 'test-net-a' in cts:
+        conn.Destroy('test-net-a')
+
+    if 'test-net-b' in cts:
+        conn.Destroy('test-net-b')
+
+    print "Cleanup uplink limit"
+    ConfigurePortod('test-net-sched', "")
