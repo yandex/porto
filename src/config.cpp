e8075088a1089fd819a18b0491b17208e4e5f00c
Alexander Kuznetsov <wwfq@yandex-team.ru>
2020-05-07 12:00:12 +0300

    config/layers: parameterize path to 'tar' binary Commit 754bb9c238bf010174b61e6a6d7f30a8d1f4c9ca upstream
    Link: https://st.yandex-team.ru/PORTO-518
    Signed-off-by: Alexander Kuznetsov <wwfq@yandex-team.ru>

--- src/config.cpp
+++ src/config.cpp
@@ -57,6 +57,7 @@ static void DefaultConfig() {
     config().mutable_daemon()->set_ro_threads(10);
     config().mutable_daemon()->set_io_threads(5);
     config().mutable_daemon()->set_vl_threads(5);
+    config().mutable_daemon()->set_tar_path("tar");
     config().mutable_daemon()->set_ptrace_protection(true);
 
     config().mutable_daemon()->set_max_clients(1000);
