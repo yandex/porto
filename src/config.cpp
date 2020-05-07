48bd6797fa1c5a9a16ff50b0d80ad2dce521a51c
Alexander Kuznetsov <wwfq@yandex-team.ru>
2020-05-07 11:54:01 +0300

    volumes: move servicing of volume requests to dedicated queue
    Prior to this change porto had single io queue for servicing both
    volume and layer requests.
    
    This prevented quick responses to lightweight 'CreateVolume'
    requests if the whole queue was filled with time-consuming
    'ImportLayer' requests.
    
    Commit 98e0c3fd100acdfea8b462488c9764509367611d upstream
    
    Link: https://st.yandex-team.ru/PORTO-518
    Signed-off-by: Alexander Kuznetsov <wwfq@yandex-team.ru>

--- src/config.cpp
+++ src/config.cpp
@@ -56,6 +56,7 @@ static void DefaultConfig() {
     config().mutable_daemon()->set_rw_threads(20);
     config().mutable_daemon()->set_ro_threads(10);
     config().mutable_daemon()->set_io_threads(5);
+    config().mutable_daemon()->set_vl_threads(5);
 
     config().mutable_daemon()->set_max_clients(1000);
     config().mutable_daemon()->set_max_clients_in_container(500);
