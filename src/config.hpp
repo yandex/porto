a08c704863e28127ffe95a6f2f55dffe685ae747
Maxim Samoylov <max7255@yandex-team.ru>
2020-07-29 04:25:18 +0300

    config: drop PROTOBUF_DEPRECATED
    Modern protobuf releases resolves this internally
    
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- src/config.hpp
+++ src/config.hpp
@@ -4,10 +4,7 @@
 
 #include "common.hpp"
 
-#undef PROTOBUF_DEPRECATED
-#define PROTOBUF_DEPRECATED __attribute__((deprecated))
 #include "config.pb.h"
-#undef PROTOBUF_DEPRECATED
 
 extern cfg::TConfig &config();
 void ReadConfigs(bool silent = false);
