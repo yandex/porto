bce6e8c96e97cd0a76f07260468a60ae0ca3471f
Alexander Kuznetsov <wwfq@yandex-team.ru>
2021-05-20 14:57:50 +0300

    porto: identify client by pid and inode of /proc/pid
    Links: https://st.yandex-team.ru/PORTO-853
    Signed-off-by: Alexander Kuznetsov wwfq@yandex-team.ru

--- src/client.hpp
+++ src/client.hpp
@@ -28,6 +28,7 @@ public:
     TCred Cred;
     TCred TaskCred;
     pid_t Pid = 0;
+    struct stat PidStat = {};
     std::string Comm;
     gid_t UserCtGroup = 0;
     std::shared_ptr<TContainer> ClientContainer;
