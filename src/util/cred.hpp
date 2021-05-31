32565912e7acb81182d7747ea987659258b02cb1
Alexander Kuznetsov <wwfq@yandex-team.ru>
2021-05-31 15:48:41 +0300

    client: not root user that non-matching owner can not manipulate containers even if client is member of "porto-containers" groups
    Links: https://st.yandex-team.ru/PORTO-863
    Signed-off-by: Alexander Kuznetsov wwfq@yandex-team.ru

--- src/util/cred.hpp
+++ src/util/cred.hpp
@@ -23,7 +23,6 @@ constexpr uid_t NoUser = (uid_t)-1;
 constexpr gid_t NoGroup = (gid_t)-1;
 
 extern gid_t PortoGroup;
-extern gid_t PortoCtGroup;
 
 static constexpr const char * CRED_POSTFORK_TAINT_MESSAGE = "Credential function may deadlock in post-fork context";
 
