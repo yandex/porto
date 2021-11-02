5f9b435126bd8d2674c1e170542a363a5847fb63
Dmitry Yakunin <zeil@yandex-team.ru>
2021-11-02 19:11:24 +0300

    blkio: fix disk resolving from client container
    Signed-off-by: Dmitry Yakunin <zeil@yandex-team.ru>
    Link: https://st.yandex-team.ru/PORTO-927

--- src/property.cpp
+++ src/property.cpp
@@ -6003,7 +6003,7 @@ public:
             std::string disk, name;
 
             error = BlkioSubsystem.ResolveDisk(CL->ClientContainer->RootPath,
-                                               index, disk);
+                                               CT->RootPath, index, disk);
             if (error)
                 return error;
             error = BlkioSubsystem.DiskName(disk, name);
