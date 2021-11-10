c254628d8073c7add62b6d7167874edbea0ab2e7
Dmitry Yakunin <zeil@yandex-team.ru>
2021-11-10 13:32:47 +0300

    Revert "blkio: fix disk resolving from client container"
    This reverts commit 12b7cb847e42a686155108a9196f4206c9eda637.

--- src/property.cpp
+++ src/property.cpp
@@ -6003,7 +6003,7 @@ public:
             std::string disk, name;
 
             error = BlkioSubsystem.ResolveDisk(CL->ClientContainer->RootPath,
-                                               CT->RootPath, index, disk);
+                                               index, disk);
             if (error)
                 return error;
             error = BlkioSubsystem.DiskName(disk, name);
