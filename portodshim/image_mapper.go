ac478b2d5063bccd5560d04358ada24acb3a0832
Lev Pantyukhin <kndrvt@yandex-team.ru>
2022-04-04 21:13:15 +0300

    portodshim: add image manipulation methods
    Signed-off-by: Lev Pantyukhin <kndrvt@yandex-team.ru>
    Link: https://st.yandex-team.ru/PORTO-978

--- portodshim/image_mapper.go
+++ portodshim/image_mapper.go
@@ -4,6 +4,12 @@ import (
 	"context"
 	"fmt"
 	v1alpha2 "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+	"syscall"
+	"time"
+)
+
+const (
+	LayersPath = "/place/porto_layers"
 )
 
 type PortodshimImageMapper struct {
@@ -12,17 +18,71 @@ type PortodshimImageMapper struct {
 
 // IMAGE SERVICE INTERFACE
 func (mapper *PortodshimImageMapper) ListImages(ctx context.Context, req *v1alpha2.ListImagesRequest) (*v1alpha2.ListImagesResponse, error) {
-	return nil, fmt.Errorf("not implemented ListImages")
+	response, err := mapper.portoClient.ListLayers2("", "")
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", getCurrentFuncName(), err)
+	}
+
+	var images []*v1alpha2.Image
+	for _, desc := range response {
+		images = append(images, &v1alpha2.Image{
+			Id:       desc.Name,
+			Username: desc.OwnerUser,
+		})
+	}
+	return &v1alpha2.ListImagesResponse{
+		Images: images,
+	}, nil
 }
 func (mapper *PortodshimImageMapper) ImageStatus(ctx context.Context, req *v1alpha2.ImageStatusRequest) (*v1alpha2.ImageStatusResponse, error) {
-	return nil, fmt.Errorf("not implemented ImageStatus")
+	response, err := mapper.portoClient.ListLayers2("", req.Image.GetImage())
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", getCurrentFuncName(), err)
+	}
+	if len(response) == 0 {
+		return nil, fmt.Errorf("%s: layer not found", getCurrentFuncName())
+	}
+	if len(response) > 1 {
+		return nil, fmt.Errorf("%s: too many layers found", getCurrentFuncName())
+	}
+
+	return &v1alpha2.ImageStatusResponse{
+		Image: &v1alpha2.Image{
+			Id:       response[0].Name,
+			Username: response[0].OwnerUser,
+		},
+	}, nil
 }
 func (mapper *PortodshimImageMapper) PullImage(ctx context.Context, req *v1alpha2.PullImageRequest) (*v1alpha2.PullImageResponse, error) {
-	return nil, fmt.Errorf("not implemented PullImage")
+	// TODO: Убрать заглушку
+	return &v1alpha2.PullImageResponse{
+		ImageRef: req.Image.GetImage(),
+	}, nil
 }
 func (mapper *PortodshimImageMapper) RemoveImage(ctx context.Context, req *v1alpha2.RemoveImageRequest) (*v1alpha2.RemoveImageResponse, error) {
-	return nil, fmt.Errorf("not implemented RemoveImage")
+	err := mapper.portoClient.RemoveLayer(req.Image.GetImage())
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", getCurrentFuncName(), err)
+	}
+	return &v1alpha2.RemoveImageResponse{}, nil
 }
 func (mapper *PortodshimImageMapper) ImageFsInfo(ctx context.Context, req *v1alpha2.ImageFsInfoRequest) (*v1alpha2.ImageFsInfoResponse, error) {
-	return nil, fmt.Errorf("not implemented ImageFsInfo")
+	stat := syscall.Statfs_t{}
+	err := syscall.Statfs(LayersPath, &stat)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", getCurrentFuncName(), err)
+	}
+
+	return &v1alpha2.ImageFsInfoResponse{
+		ImageFilesystems: []*v1alpha2.FilesystemUsage{
+			{
+				Timestamp: time.Now().UnixNano(),
+				FsId: &v1alpha2.FilesystemIdentifier{
+					Mountpoint: LayersPath,
+				},
+				UsedBytes:  &v1alpha2.UInt64Value{Value: (stat.Blocks - stat.Bfree) * uint64(stat.Bsize)},
+				InodesUsed: &v1alpha2.UInt64Value{Value: stat.Files - stat.Ffree},
+			},
+		},
+	}, nil
 }
