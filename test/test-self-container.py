6a9b8d1f27b705703a209eabe19e2af2ac908f48
Maxim Samoylov <max7255@yandex-team.ru>
2022-07-15 18:56:47 +0300

    test: fix self-container test stability issue
    Signed-off-by: Maxim Samoylov <max7255@yandex-team.ru>

--- test/test-self-container.py
+++ test/test-self-container.py
@@ -43,21 +43,32 @@ ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=tes
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE), '/porto/test/test\n')
 
 # Find
-ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=',
-                                'command=' + portoctl + ' find 1'],
+
+tmp_path = '/tmp/find_script.sh'
+
+try:
+	f = open(tmp_path, "w")
+	f.write("#!/bin/bash\nsleep 1\n" + portoctl + ' find 1\n')
+	f.close()
+	os.chmod(tmp_path, 0755)
+
+	ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=',
+                                'command=bash -c \"sleep 1 ; ' + portoctl + ' find 1\"'],
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE), 'self\n')
 
-ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=test/',
-                                'command=' + portoctl + ' find 1'],
+	ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=test/',
+                                'command=bash -c \"sleep 1 ; ' + portoctl + ' find 1\"'],
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE), 'self\n')
 
-ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=',
-                                'command=' + portoctl + ' exec self/test porto_namespace= command=\"' + portoctl + ' find 1\"'],
+	ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=',
+                                'command=' + portoctl + ' exec self/test porto_namespace= command=\"' + tmp_path + '\"'],
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE), 'self\n')
 
-ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=test/',
-                                'command=' + portoctl + ' exec test porto_namespace= command=\"' + portoctl + ' find 1\"'],
+	ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'porto_namespace=test/',
+                                'command=' + portoctl + ' exec test porto_namespace= command=\"' + tmp_path + '\"'],
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE), 'self\n')
+finally:
+	os.unlink(tmp_path)
 
 # Isolate
 ExpectEq(subprocess.check_output([portoctl, 'exec', 'test', 'enable_porto=isolate',
