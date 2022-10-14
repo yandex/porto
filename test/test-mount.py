39eac2b3c6f78a6d705c59e21e57ab89c5780efc
Lev Pantyukhin <kndrvt@yandex-team.ru>
2022-10-14 13:33:08 +0300

    test: add file binding case to mount test
    Signed-off-by: Lev Pantyukhin <kndrvt@yandex-team.ru>

--- test/test-mount.py
+++ test/test-mount.py
@@ -6,7 +6,7 @@ from test_common import *
 # now allow suid binaries in linked volumes
 nosuid = False
 
-c = porto.Connection(timeout=10)
+conn = porto.Connection(timeout=10)
 
 def CheckBaseMounts(mnt, chroot=True):
     Expect('rw' in mnt['/']['flag'])
@@ -28,14 +28,14 @@ def CheckBaseMounts(mnt, chroot=True):
     Expect('/sys/fs/cgroup/cpu' not in mnt)
     Expect('/sys/fs/cgroup/memory' not in mnt)
 
-w = c.Create("w", weak=True)
+w = conn.Create("w", weak=True)
 
-root_volume = c.CreateVolume(backend='overlay', layers=['ubuntu-precise'], containers='w')
-test_volume = c.CreateVolume(backend='plain', containers='w')
-ro_volume = c.CreateVolume(backend='plain', read_only='true', containers='w')
+root_volume = conn.CreateVolume(backend='overlay', layers=['ubuntu-precise'], containers='w')
+test_volume = conn.CreateVolume(backend='plain', containers='w')
+ro_volume = conn.CreateVolume(backend='plain', read_only='true', containers='w')
 
 os.mkdir(test_volume.path + "/sub")
-sub_volume = c.CreateVolume(path=test_volume.path + "/sub", backend='plain', containers='w')
+sub_volume = conn.CreateVolume(path=test_volume.path + "/sub", backend='plain', containers='w')
 
 host_mnt = ParseMountinfo()
 
@@ -52,19 +52,19 @@ Expect('nodev' in host_mnt[ro_volume.path]['flag'])
 Expect('nosuid' in host_mnt[ro_volume.path]['flag'] if nosuid else True)
 
 # no chroot
-a = c.Run("a")
+a = conn.Run("a")
 mnt = ParseMountinfo(a['root_pid'])
 CheckBaseMounts(mnt, chroot=False)
 a.Destroy()
 
 # simple chroot
-a = c.Run("a", root=root_volume.path)
+a = conn.Run("a", root=root_volume.path)
 mnt = ParseMountinfo(a['root_pid'])
 CheckBaseMounts(mnt)
 a.Destroy()
 
 # container binds
-a = c.Run("a", root=root_volume.path, bind="{0} /test; {0} /test_ro ro,rec; {1} /ro_vol".format(test_volume.path, ro_volume.path))
+a = conn.Run("a", root=root_volume.path, bind="{0} /test; {0} /test_ro ro,rec; {1} /ro_vol".format(test_volume.path, ro_volume.path))
 mnt = ParseMountinfo(a['root_pid'])
 CheckBaseMounts(mnt)
 Expect('/' in mnt)
@@ -97,7 +97,7 @@ Expect('ro' in mnt['/ro_vol']['flag'])
 
 # container binds propagation
 os.mkdir(test_volume.path + "/sub2")
-test_sub2 = c.CreateVolume(path=test_volume.path + "/sub2", backend='plain', containers='a')
+test_sub2 = conn.CreateVolume(path=test_volume.path + "/sub2", backend='plain', containers='a')
 mnt = ParseMountinfo(a['root_pid'])
 Expect('/test/sub2' in mnt)
 Expect('rw' in mnt['/test/sub2']['flag'])
@@ -110,7 +110,7 @@ a.Destroy()
 os.mkdir(root_volume.path + "/symlink_dst")
 os.symlink("/symlink_dst", root_volume.path + "/symlink_src")
 
-a = c.Run("a", root=root_volume.path, bind="{0} /symlink_src".format(test_volume.path))
+a = conn.Run("a", root=root_volume.path, bind="{0} /symlink_src".format(test_volume.path))
 mnt = ParseMountinfo(a['root_pid'])
 CheckBaseMounts(mnt)
 Expect('/' in mnt)
@@ -122,12 +122,12 @@ ExpectEq(mnt['/symlink_dst']['master'], host_mnt[test_volume.path]['shared'])
 a.Destroy()
 
 #backend bind and rbind
-a = c.Run("a", root=root_volume.path)
+a = conn.Run("a", root=root_volume.path)
 os.mkdir(root_volume.path + "/test_rbind")
 os.mkdir(root_volume.path + "/test_ro_rbind")
-test_bind = c.CreateVolume(path=root_volume.path + "/test", backend='bind', storage=test_volume.path, containers='a')
-test_rbind = c.CreateVolume(path=root_volume.path + "/test_rbind", backend='rbind', storage=test_volume.path, containers='a')
-test_ro_rbind = c.CreateVolume(path=root_volume.path + "/test_ro_rbind", backend='rbind', read_only='true', storage=test_volume.path, containers='a')
+test_bind = conn.CreateVolume(path=root_volume.path + "/test", backend='bind', storage=test_volume.path, containers='a')
+test_rbind = conn.CreateVolume(path=root_volume.path + "/test_rbind", backend='rbind', storage=test_volume.path, containers='a')
+test_ro_rbind = conn.CreateVolume(path=root_volume.path + "/test_ro_rbind", backend='rbind', read_only='true', storage=test_volume.path, containers='a')
 
 mnt = ParseMountinfo()
 
@@ -178,7 +178,7 @@ Expect('master' in mnt['/test_ro_rbind/sub'])
 Expect('shared' in mnt['/test_ro_rbind/sub'])
 
 # backend bind and rbind propagation
-test_sub2 = c.CreateVolume(path=test_volume.path + "/sub2", backend='plain', containers='a')
+test_sub2 = conn.CreateVolume(path=test_volume.path + "/sub2", backend='plain', containers='a')
 mnt = ParseMountinfo(a['root_pid'])
 
 Expect('/test/sub2' in mnt)
@@ -202,9 +202,9 @@ Expect('/test_ro_rbind/sub2_link' not in mnt)
 a.Destroy()
 
 # volumes in chroot
-a = c.Run("a", root=root_volume.path, bind="{} /bin/portoctl ro".format(portoctl))
-a1 = c.Run("a/a1", wait=60, command="portoctl vcreate /test backend=plain")
-a2 = c.Run("a/a2", wait=60, command="portoctl vcreate /test_ro backend=plain read_only=true")
+a = conn.Run("a", root=root_volume.path, bind="{} /bin/portoctl ro".format(portoctl))
+a1 = conn.Run("a/a1", wait=60, command="portoctl vcreate /test backend=plain")
+a2 = conn.Run("a/a2", wait=60, command="portoctl vcreate /test_ro backend=plain read_only=true")
 
 mnt = ParseMountinfo()
 Expect(root_volume.path + '/test' in mnt)
@@ -222,7 +222,7 @@ a.Destroy()
 
 
 # volume links
-a = c.Create("a", weak=True)
+a = conn.Create("a", weak=True)
 a['root'] = root=root_volume.path
 test_volume.Link(a, target="/test_ro", read_only=True)
 ro_volume.Link(a, target="/ro_vol", read_only=True)
@@ -307,4 +307,35 @@ mnt = ParseMountinfo()
 Expect(root_volume.path + '/symlink_src2/sub_dir' in mnt)
 
 a.Destroy()
+
+
+# mount bind file
+root_a = conn.CreateVolume(containers='w')
+os.mkdir(root_a.path + "/root")
+root_b = conn.CreateVolume(root_a.path + "/root", backend='overlay', layers=['ubuntu-precise'], containers='w')
+
+a = conn.Run("a", root=root_a.path, weak=True)
+b = conn.Run("a/b", root="/root", weak=True)
+
+with open("/tmp/kek", "w") as file:
+    file.write("kek")
+with open(root_b.path + "/tmp/lol", "a") as file:
+    file.write("lol")
+bind_volume = conn.CreateVolume("", backend="bind", storage="/tmp/kek", containers='w')
+
+ExpectEq(subprocess.check_output(["cat", "/tmp/kek"]), b"kek")
+ExpectEq(subprocess.check_output(["cat", root_b.path + "/tmp/lol"]), b"lol")
+
+conn.LinkVolume(bind_volume.path, "a/b", "/tmp/kek")
+conn.LinkVolume(bind_volume.path, "a/b", "/tmp/lol")
+
+c = conn.Run("a/b/c", command="cat /tmp/kek", wait=5)
+
+ExpectEq(c['exit_code'], "0")
+ExpectEq(c['stdout'], "kek")
+
+ExpectEq(subprocess.check_output(["cat", "/tmp/kek"]), b"kek")
+ExpectEq(subprocess.check_output(["cat", root_b.path + "/tmp/lol"]), b"kek")
+ExpectEq(subprocess.check_output(["cat", root_b.path + "/tmp/kek"]), b"kek")
+
 w.Destroy()
